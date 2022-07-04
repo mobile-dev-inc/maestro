6670096c93fe63af8aec2b6c49419ed030c4695d
Amanjeet Singh <amanjeetsingh150@gmail.com>
2022-07-05 03:38:23 +0530

    Cleans up root level gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-group = "dev.mobile"
-version = "1.0.0"
-
-extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
-
 buildscript {
     repositories {
         google()
@@ -18,11 +13,6 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.19.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
-
-    `java-library`
-    `maven-publish`
-
-    signing
 }
 
 repositories {
@@ -46,66 +36,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-    }
-}
-
-tasks {
-    javadoc {
-        if (JavaVersion.current().isJava9Compatible) {
-            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-        }
-    }
-
-    clean {
-        delete("${rootProject.buildDir}")
-    }
-
-    test {
-        useJUnitPlatform()
-    }
-}
-
-signing {
-    setRequired({
-        (project.extra["isReleaseVersion"] as Boolean) && gradle.taskGraph.hasTask("publish")
-    })
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("main") {
-            from(components["java"])
-
-            afterEvaluate {
-                artifactId = tasks.jar.get().archiveBaseName.get()
-            }
-
-            pom {
-                name.set("Conductor")
-                description.set("A library to automate in-app navigation")
-                url.set("https://www.mobile.dev/")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("mobile-dev-inc")
-                        name.set("mobile.dev Inc.")
-                        email.set("support@mobile.dev")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/mobile-dev-inc/conductor.git")
-                    developerConnection.set("scm:git:ssh://github.com/mobile-dev-inc/conductor.git")
-                    url.set("https://www.mobile.dev/")
-                }
-            }
-        }
     }
 }
