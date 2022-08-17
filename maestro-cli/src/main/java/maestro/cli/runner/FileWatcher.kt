2178f8313d2453571c314cb67c74fdb9b1de9242
Leland Takamine <leland.takamine@gmail.com>
2022-08-17 16:43:35 -0700

    Fix file watch capability

--- maestro-cli/src/main/java/maestro/cli/runner/FileWatcher.kt
+++ maestro-cli/src/main/java/maestro/cli/runner/FileWatcher.kt
@@ -6,6 +6,7 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 import java.nio.file.StandardWatchEventKinds
 import java.nio.file.WatchKey
+import kotlin.io.path.absolute
 
 class FileWatcher {
 
@@ -16,7 +17,7 @@ class FileWatcher {
         watchKeys.forEach(WatchKey::cancel)
         watchKeys.clear()
 
-        val paths = files.map { it.toPath() }
+        val paths = files.map { it.toPath().absolute() }
 
         paths.forEach { path ->
             val watchKey = path.parent.register(
