dba125cacf127783438f2abffa538c9aeff8826a
Dima Zaytsev <dmitry.zaicew@gmail.com>
2022-08-22 16:11:25 +0200

    Fix: injectEnv did not work with init flow

--- maestro-orchestra-models/src/main/java/maestro/orchestra/MaestroConfig.kt
+++ maestro-orchestra-models/src/main/java/maestro/orchestra/MaestroConfig.kt
@@ -1,5 +1,7 @@
 package maestro.orchestra
 
+import maestro.orchestra.util.Env.injectEnv
+
 // Note: The appId config is only a yaml concept for now. It'll be a larger migration to get to a point
 // where appId is part of MaestroConfig (and factored out of MaestroCommands - eg: LaunchAppCommand).
 data class MaestroConfig(
@@ -7,9 +9,28 @@ data class MaestroConfig(
     val name: String? = null,
     val initFlow: MaestroInitFlow? = null,
     val ext: Map<String, Any?> = emptyMap(),
-)
+) {
+
+    fun injectEnv(env: Map<String, String>): MaestroConfig {
+        return copy(
+            appId = appId?.injectEnv(env),
+            name = name?.injectEnv(env),
+            initFlow = initFlow?.injectEnv(env),
+        )
+    }
+
+}
 
 data class MaestroInitFlow(
     val appId: String,
     val commands: List<MaestroCommand>,
-)
+) {
+
+    fun injectEnv(env: Map<String, String>): MaestroInitFlow {
+        return copy(
+            appId = appId.injectEnv(env),
+            commands = commands.map { it.injectEnv(env) },
+        )
+    }
+
+}
