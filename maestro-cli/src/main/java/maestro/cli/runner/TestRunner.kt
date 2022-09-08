9f6da45beb18ac992fadfaaeba8e592fe12b3d21
Dima Zaytsev <dmitry.zaicew@gmail.com>
2022-09-08 14:08:16 +0200

    More descriptive message

--- maestro-cli/src/main/java/maestro/cli/runner/TestRunner.kt
+++ maestro-cli/src/main/java/maestro/cli/runner/TestRunner.kt
@@ -107,6 +107,7 @@ object TestRunner {
         return YamlCommandReader.getConfig(commands)?.initFlow
     }
 
+    @Suppress("MaxLineLength")
     private fun <T> runCatching(
         view: ResultView,
         block: () -> T,
@@ -118,7 +119,7 @@ object TestRunner {
                 is SyntaxError -> "Could not parse Flow file:\n\n${e.message}"
                 is NoInputException -> "No commands found in Flow file"
                 is InvalidInitFlowFile -> "initFlow file is invalid: ${e.initFlowPath}"
-                is UnicodeNotSupportedError -> "Unicode character input is not supported: ${e.text}"
+                is UnicodeNotSupportedError -> "Unicode character input is not supported: ${e.text}. Please use ASCII characters. Follow the issue: https://github.com/mobile-dev-inc/maestro/issues/146"
                 else -> e.stackTraceToString()
             }
 
