f26e17b508316083ff06e4e8b71b9a34a6c59cb8
depa panjie purnama <depa.article@gmail.com>
2022-10-06 17:10:54 +0700

    fix: typo (#258)
    * fix typo
    
    * fix another typo

--- maestro-orchestra-models/src/main/java/maestro/orchestra/util/InputRandomTextHelper.kt
+++ maestro-orchestra-models/src/main/java/maestro/orchestra/util/InputRandomTextHelper.kt
@@ -5,7 +5,7 @@ object InputRandomTextHelper {
     private const val CHARSET_NUMBER = "0123456789"
     private const val CHARSET_NUMBER_WITHOUT_ZERO = "123456789"
     private val LIST_POPULAR_LAST_NAME = arrayOf("Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez")
-    private val LIST_POPULAR_FIST_NAME = arrayOf(
+    private val LIST_POPULAR_FIRST_NAME = arrayOf(
         "Liam",
         "Olivia",
         "Noah",
@@ -30,19 +30,19 @@ object InputRandomTextHelper {
     private val LIST_POPULAR_EMAIL_DOMAIN = arrayOf("gmail.com", "yahoo.com", "hotmail.com", "aol.com", "msn.com", "outlook.com")
 
     /**
-     * Returns random person name format: FistName LastName
+     * Returns random person name format: FirstName LastName
      */
     fun randomPersonName() = String.format(
         "%s %s",
-        LIST_POPULAR_FIST_NAME.random(), LIST_POPULAR_LAST_NAME.random()
+        LIST_POPULAR_FIRST_NAME.random(), LIST_POPULAR_LAST_NAME.random()
     )
 
     /**
-     * Returns random email address with format: fistName_lastName_randomTex@emailDomain
+     * Returns random email address with format: firstName_lastName_randomText@emailDomain
      */
     fun randomEmail() = String.format(
         "%s_%s_%s@%s",
-        LIST_POPULAR_FIST_NAME.random(),
+        LIST_POPULAR_FIRST_NAME.random(),
         LIST_POPULAR_LAST_NAME.random(),
         getRandomText(length = 4),
         LIST_POPULAR_EMAIL_DOMAIN.random(),
