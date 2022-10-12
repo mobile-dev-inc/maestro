824aef668af122ed81511428bbc5c3a3bef21b40
Axel Niklasson <axel.r.niklasson@gmail.com>
2022-10-12 12:10:32 +0200

    breaking change: change upload name to be an option instead of positional argument (#266)
    * breaking change: change upload name to be an option instead of positional argument
    
    * Update ./maestro script to correctly pass arguments to maestro-cli executable

--- maestro-cli/src/main/java/maestro/cli/command/UploadCommand.kt
+++ maestro-cli/src/main/java/maestro/cli/command/UploadCommand.kt
@@ -37,9 +37,6 @@ import kotlin.io.path.absolute
 )
 class UploadCommand : Callable<Int> {
 
-    @CommandLine.Parameters(description = ["The name of this upload"])
-    private lateinit var uploadName: String
-
     @CommandLine.Parameters(description = ["App binary to run your Flows against"])
     private lateinit var appFile: File
 
@@ -69,6 +66,9 @@ class UploadCommand : Callable<Int> {
 
     @Option(order = 7, names = ["-e", "--env"], description = ["Environment variables to inject into your Flows"])
     private var env: Map<String, String> = emptyMap()
+
+    @Option(order = 8, names = ["--name"], description = ["Name of the upload"])
+    private var uploadName: String? = null
 
     private lateinit var client: ApiClient
 
