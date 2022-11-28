0e43a61dbc9d49c3337dc213c07804958d5d4c1c
Amanjeet Singh <amanjeetsingh150@gmail.com>
2022-11-28 21:11:30 +0530

    Setup `maestro-ios-driver` project

---
+++ maestro-driver-ios/maestro-driver-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maestro-driver-ios
+//
+//  Created by Amanjeet Singh on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
