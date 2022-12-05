c5101be6f851bf665fe9b0d8b54bd4cf99dd75a8
Leland Takamine <leland.takamine@gmail.com>
2022-12-05 13:50:16 -0800

    Simplify MaestroStudio class

--- maestro-studio/server/src/main/java/maestro/studio/MaestroStudio.kt
+++ maestro-studio/server/src/main/java/maestro/studio/MaestroStudio.kt
@@ -7,29 +7,20 @@ import org.http4k.routing.ResourceLoader
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.routing.singlePageApp
-import org.http4k.server.Http4kServer
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
-class MaestroStudio private constructor(private val server: Http4kServer) {
+object MaestroStudio {
 
-    fun stop() {
-        server.stop()
-    }
-
-    companion object {
-
-        fun start(port: Int): MaestroStudio {
-            val api = routes(
-                "/hello" bind GET to {Response(OK).body("HELLO!")}
-            )
-            val app = routes(
-                "/api" bind api,
-                singlePageApp(ResourceLoader.Classpath("/web")),
-            )
-            val server = app.asServer(Undertow(port))
-            server.start()
-            return MaestroStudio(server)
-        }
+    fun start(port: Int) {
+        val api = routes(
+            "/hello" bind GET to { Response(OK).body("HELLO!") }
+        )
+        val app = routes(
+            "/api" bind api,
+            singlePageApp(ResourceLoader.Classpath("/web")),
+        )
+        val server = app.asServer(Undertow(port))
+        server.start()
     }
 }
