bf30ef04005bc9859c9b8ca6e4ceb7cc8fd6e9e9
Leland Takamine <leland.takamine@gmail.com>
2022-12-06 04:56:50 -0800

    Add screenshot

--- maestro-studio/server/src/main/java/maestro/studio/MaestroStudio.kt
+++ maestro-studio/server/src/main/java/maestro/studio/MaestroStudio.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.files
 import io.ktor.server.http.content.singlePageApplication
 import io.ktor.server.http.content.static
 import io.ktor.server.http.content.staticRootFolder
@@ -12,11 +13,17 @@ import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import maestro.Maestro
-import java.io.File
+import maestro.TreeNode
 import java.nio.file.Path
+import java.util.UUID
 import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
 import kotlin.io.path.createTempDirectory
+
+data class Hierarchy(
+    val screenshot: String,
+    val tree: TreeNode,
+)
 
 object MaestroStudio {
 
@@ -26,14 +33,18 @@ object MaestroStudio {
         embeddedServer(Netty, port = port) {
             routing {
                 get("/api/hierarchy") {
-                    val hierarchy = jacksonObjectMapper()
+                    val tree = maestro.viewHierarchy().root
+                    val screenshot = takeScreenshot(maestro)
+                    val hierarchy = Hierarchy(screenshot, tree)
+                    val response = jacksonObjectMapper()
                         .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                         .writerWithDefaultPrettyPrinter()
-                        .writeValueAsString(maestro.viewHierarchy().root)
-                    call.respondText(hierarchy)
+                        .writeValueAsString(hierarchy)
+                    call.respondText(response)
                 }
                 static("/screenshot") {
                     staticRootFolder = screenshotDir.toFile()
+                    files(".")
                 }
                 singlePageApplication {
                     useResources = true
@@ -42,6 +53,14 @@ object MaestroStudio {
                 }
             }
         }.start()
+    }
+
+    private fun takeScreenshot(maestro: Maestro): String {
+        val name = "${UUID.randomUUID()}.png"
+        val screenshotFile = screenshotDir.resolve(name).toFile()
+        screenshotFile.deleteOnExit()
+        maestro.takeScreenshot(screenshotFile)
+        return "/screenshot/$name"
     }
 
     private fun getScreenshotDir(): Path {
--- maestro-studio/web/src/App.tsx
+++ maestro-studio/web/src/App.tsx
@@ -1,18 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+type TreeNode = {
+  attributes: {[key: string]: string}
+  children: TreeNode[]
+  clickable: boolean | undefined
+  enabled: boolean | undefined
+  focused: boolean | undefined
+  checked: boolean | undefined
+  selected: boolean | undefined
+}
+
+type Hierarchy = {
+  screenshot: string
+  tree: TreeNode
+}
+
 function App() {
-  const [hierarchy, setHierarchy] = useState<string>()
+  const [hierarchy, setHierarchy] = useState<Hierarchy>()
   useEffect(() => {
     (async () => {
       const response = await fetch('/api/hierarchy')
-      const hierarch = await response.text()
-      setHierarchy(hierarch)
+      const hierarchy: Hierarchy = await response.json()
+      setHierarchy(hierarchy)
     })()
   }, [])
+  if (!hierarchy) {
+    return (
+      <div>Loading...</div>
+    )
+  }
   return (
     <div className="App">
-      {hierarchy}
+      <img src={hierarchy.screenshot} alt="screenshot"/>
+      <p>{JSON.stringify(hierarchy.tree)}</p>
     </div>
   );
 }
