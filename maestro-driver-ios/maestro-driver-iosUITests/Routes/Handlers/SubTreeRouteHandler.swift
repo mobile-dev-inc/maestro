a262f658c6d6222ba57c932648a762757bfac849
Amanjeet Singh <amanjeetsingh150@gmail.com>
2022-12-07 14:31:34 +0530

    Include logging in `SubTreeRouteHandler` and return 4xx
    - Return bad request instead of throwing exceptions

--- maestro-driver-ios/maestro-driver-iosUITests/Routes/Handlers/SubTreeRouteHandler.swift
+++ maestro-driver-ios/maestro-driver-iosUITests/Routes/Handlers/SubTreeRouteHandler.swift
@@ -1,25 +1,44 @@
 import FlyingFox
 import XCTest
+import os
 
 class SubTreeRouteHandler: RouteHandler {
+    
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "SubTreeRouteHandler")
+    
     func handle(request: HTTPRequest) async throws -> FlyingFox.HTTPResponse {
         guard let appId = request.query["appId"] else {
+            logger.error("Requested view hierarchy for an invalid appId")
             return HTTPResponse(statusCode: HTTPStatusCode.badRequest)
         }
-        let viewHierarchyDictionaryResult = await MainActor.run {
-            try? XCUIApplication(bundleIdentifier: appId).snapshot().dictionaryRepresentation
+        let viewHierarchyDictionaryResult = await MainActor.run { () -> [XCUIElement.AttributeName : Any]? in
+            do {
+                logger.info("Trying to capture hierarchy snapshot for \(appId)")
+                return try XCUIApplication(bundleIdentifier: appId).snapshot().dictionaryRepresentation
+            } catch let error {
+                let message = error.localizedDescription
+                logger.error("Snapshot failure, cannot return view hierarchy due to \(message)")
+                return nil
+            }
         }
         guard let viewHierarchyDictionary = viewHierarchyDictionaryResult else {
-            print("Cannot return view hierarchy, throwing exception..")
-            throw ServerError.ApplicationSnapshotFailure
+            let jsonString = """
+             { "errorMessage" : "Snapshot failure while getting view hierarchy" }
+            """
+            let errorData = Data(jsonString.utf8)
+            return HTTPResponse(statusCode: HTTPStatusCode.badRequest, body: errorData)
         }
         guard let hierarchyJsonData = try? JSONSerialization.data(
             withJSONObject: viewHierarchyDictionary,
             options: .prettyPrinted
         ) else {
-            print("Serialization of view hierarchy failed")
-            throw ServerError.SnapshotSerializeFailure
+            logger.error("Serialization of view hierarchy failed \(viewHierarchyDictionary.debugDescription)")
+            let jsonString = """
+             { "errorMessage" : "Not able to serialize the view hierarchy response" }
+            """
+            return HTTPResponse(statusCode: .badRequest, body: Data(jsonString.utf8))
         }
+        logger.info("Successfully got view hierarchy for \(appId)")
         return HTTPResponse(statusCode: .ok, body: hierarchyJsonData)
     }
 }
