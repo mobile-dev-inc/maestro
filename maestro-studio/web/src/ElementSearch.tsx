800e0b9f3036286e29a1fa133a5eabd668bb2d92
Leland Takamine <leland.takamine@gmail.com>
2022-12-08 11:17:07 -0800

    Add list

--- maestro-studio/web/src/ElementSearch.tsx
+++ maestro-studio/web/src/ElementSearch.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { DeviceScreen, UIElement } from './models';
 
+const compare = (a: string | undefined, b: string | undefined) => {
+  if (!a) return b ? 1 : 0
+  if (!b) return -1
+  return a.localeCompare(b)
+}
+
 const ElementSearch = ({deviceScreen, selectedElement, onElementSelected, hoveredElement, onElementHovered}: {
   deviceScreen: DeviceScreen
   selectedElement: UIElement | null
@@ -9,16 +15,18 @@ const ElementSearch = ({deviceScreen, selectedElement, onElementSelected, hovere
   onElementHovered: (element: UIElement | null) => void
 }) => {
   const sortedElements = deviceScreen.elements.sort((a, b) => {
-    if (!a.text) return 1
-    if (!b.text) return -1
-    return a.text.localeCompare(b.text)
+    return compare(a.text, b.text) || compare(a.resourceId, b.resourceId)
   })
   return (
     <div className="flex flex-col h-full">
       <div className="font-bold text-lg">UI Elements</div>
       <div className="overflow-scroll">
         {sortedElements.map(element => (
-          <div>{element.text}</div>
+          <div className="flex gap-3 items-center p-5 rounded border border overflow-hidden">
+            <span className="whitespace-nowrap overflow-hidden text-ellipsis">{element.text}</span>
+            <div className="flex-1"/>
+            <span className="whitespace-nowrap overflow-hidden  text-ellipsis">{element.resourceId}</span>
+          </div>
         ))}
       </div>
     </div>
