9dfa884332837b2ff16e467b5d23116aa60ba1eb
Leland Takamine <leland.takamine@gmail.com>
2022-12-07 18:22:51 -0800

    PageSwitcher api

--- maestro-studio/web/src/Inspect.tsx
+++ maestro-studio/web/src/Inspect.tsx
@@ -2,6 +2,31 @@ import { AnnotatedScreenshot } from './AnnotatedScreenshot';
 import React, { useState } from 'react';
 import { DeviceScreen, UIElement } from './models';
 
+const CloseIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  )
+}
+
+export const Banner = ({left, right}: {
+  left: string
+  right: string
+}) => {
+  return (
+    <div className="flex justify-between items-center font-bold p-2 pr-5 rounded bg-blue-100 border border-blue-500">
+      <div className="flex gap-3 items-center">
+        <div className="flex justify-center p-2 rounded items-center hover:bg-blue-900/20 active:bg-blue-900/40">
+          <CloseIcon />
+        </div>
+        <span>{left}</span>
+      </div>
+      <span>{right}</span>
+    </div>
+  )
+}
+
 const Inspect = ({ deviceScreen }: {
   deviceScreen: DeviceScreen
 }) => {
--- maestro-studio/web/src/PageSwitcher.tsx
+++ maestro-studio/web/src/PageSwitcher.tsx
@@ -1,8 +1,50 @@
-const PageSwitcher = () => {
+import { AnimatePresence, motion } from "framer-motion"
+import { ReactElement } from 'react';
+
+
+const PageSwitcher = ({ children, banner }: {
+  banner: ReactElement | null
+  children: [ReactElement, ReactElement | null]
+}) => {
+  const animationDuration = 0.06
   return (
-    <div>
-      Page Switcher
-    </div>
+    <motion.div
+      className="flex flex-col gap-2 w-full h-full"
+    >
+      {banner ? (
+        <motion.div
+          initial={{ opacity: 0, scale: .9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: animationDuration }}
+        >
+          {banner}
+        </motion.div>
+      ) : null}
+      <motion.div
+        className="w-full h-full relative rounded-lg drop-shadow-md border bg-white shadow-l overflow-clip"
+        layout
+        transition={{ duration: animationDuration }}
+      >
+        <motion.div
+          className="absolute w-full h-full bg-white"
+        >
+          {children[0]}
+        </motion.div>
+        <AnimatePresence>
+          {children[1] ? (
+            <motion.div
+              className="absolute w-full h-full bg-white"
+              initial={{ opacity: 0, translateY: '20px' }}
+              animate={{ opacity: 1, translateY: 0 }}
+              exit={{ opacity: 0, translateY: '20px' }}
+              transition={{ duration: animationDuration }}
+            >
+              {children[1]}
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
+      </motion.div>
+    </motion.div>
   )
 }
 
