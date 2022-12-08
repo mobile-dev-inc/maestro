a385304115b8bf0e41ead333c5d42b98f983f769
Leland Takamine <leland.takamine@gmail.com>
2022-12-08 13:04:52 -0800

    Add search box

--- maestro-studio/web/src/ElementSearch.tsx
+++ maestro-studio/web/src/ElementSearch.tsx
@@ -1,11 +1,19 @@
 import { motion } from 'framer-motion';
-import React from 'react';
+import React, { useState } from 'react';
 import { DeviceScreen, UIElement } from './models';
 
 const compare = (a: string | undefined, b: string | undefined) => {
   if (!a) return b ? 1 : 0
   if (!b) return -1
   return a.localeCompare(b)
+}
+
+const SearchIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+      <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>
+  )
 }
 
 const ElementSearch = ({deviceScreen, selectedElement, onElementSelected, hoveredElement, onElementHovered}: {
@@ -15,12 +23,35 @@ const ElementSearch = ({deviceScreen, selectedElement, onElementSelected, hovere
   hoveredElement: UIElement | null
   onElementHovered: React.Dispatch<React.SetStateAction<UIElement | null>>
 }) => {
-  const sortedElements = deviceScreen.elements.sort((a, b) => {
+  const [query, setQuery] = useState("");
+
+  const filteredElements = deviceScreen.elements.filter(element => {
+    return !query || element.text?.toLowerCase().includes(query.toLowerCase()) || element.resourceId?.toLowerCase().includes(query.toLowerCase())
+  })
+
+  const sortedElements = filteredElements.sort((a, b) => {
+    const aTextPrefixMatch = query && a.text?.toLowerCase().startsWith(query.toLowerCase())
+    const bTextPrefixMatch = query && b.text?.toLowerCase().startsWith(query.toLowerCase())
+    if (aTextPrefixMatch && !bTextPrefixMatch) return -1
+    if (bTextPrefixMatch && !aTextPrefixMatch) return 1
     return compare(a.text, b.text) || compare(a.resourceId, b.resourceId)
   })
+
   return (
-    <div className="flex flex-col h-full">
-      <div className="font-bold text-lg">UI Elements</div>
+    <div className="flex flex-col h-full gap-3">
+      <div className="font-bold text-lg mb-5">UI Elements</div>
+      <div className="flex relative">
+        <div className="flex items-center pl-5 absolute pointer-events-none inset-y-0">
+          <SearchIcon className="w-6 text-slate-500"/>
+        </div>
+        <input
+          type="search"
+          placeholder="Search by element text or ID"
+          className="rounded w-full p-4 pl-14 bg-slate-100 focus:outline-slate-900"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+      </div>
       <div className="flex flex-col overflow-scroll ">
         {sortedElements.map(element => (
           <div
