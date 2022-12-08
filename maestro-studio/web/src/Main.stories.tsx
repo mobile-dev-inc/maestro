d7eb6c9a972444cef3f28c6ab4e17bae590864e1
Leland Takamine <leland.takamine@gmail.com>
2022-12-08 15:51:04 -0800

    Add MainStory

---
+++ maestro-studio/web/src/Main.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { DeviceScreen } from './models';
+import { sampleElements, sampleScreenshot } from './fixtures';
+import Inspect from './Inspect';
+import Main from './Main';
+
+export default {
+  title: 'Main',
+  parameters: {
+    layout: 'fullscreen'
+  }
+}
+
+const deviceScreen: DeviceScreen = {
+  screenshot: sampleScreenshot,
+  width: 1080,
+  height: 2340,
+  elements: sampleElements,
+}
+
+export const MainStory = () => {
+  const getDeviceScreen = async () => {
+    await new Promise(resolve => setTimeout(resolve, 500))
+    return deviceScreen
+  }
+  return (
+    <Main getDeviceScreen={getDeviceScreen} />
+  )
+}
