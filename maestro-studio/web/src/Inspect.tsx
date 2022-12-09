b0cb0ece7151395d82e1237bb7e2f70bfe8db05a
Leland Takamine <leland.takamine@gmail.com>
2022-12-09 11:16:39 -0800

    Add footer

--- maestro-studio/web/src/Inspect.tsx
+++ maestro-studio/web/src/Inspect.tsx
@@ -7,6 +7,25 @@ import ElementSearch from './ElementSearch';
 import { motion } from 'framer-motion';
 import Examples from './Examples';
 
+const Footer = ({selectedElement, hoveredElement}: {
+  selectedElement: UIElement | null
+  hoveredElement: UIElement | null
+}) => {
+  const content = selectedElement ? (
+    <span>asdf</span>
+  ) : null
+  return (
+    <motion.div
+      className="flex items-center px-3 bg-slate-600 h-8 text-white"
+      initial={{ translateY: '40px' }}
+      animate={{ translateY: 0 }}
+      transition={{ ease: 'easeOut', duration: .05 }}
+    >
+      {content}
+    </motion.div>
+  )
+}
+
 const Inspect = ({ deviceScreen }: {
   deviceScreen: DeviceScreen
 }) => {
@@ -27,27 +46,35 @@ const Inspect = ({ deviceScreen }: {
   
   return (
     <motion.div
-      className="App flex h-full gap-10 p-10 overflow-hidden"
-      initial={{ scale: .97, opacity: 0 }}
-      animate={{ scale: 1, opacity: 1 }}
-      transition={{ duration: .08 }}
+      className="flex flex-col overflow-hidden"
     >
-      <AnnotatedScreenshot
-        deviceScreen={deviceScreen}
-        onElementHovered={setHoveredElement}
-        hoveredElement={hoveredElement}
-        onElementSelected={setSelectedElement}
-        selectedElement={selectedElement}
-      />
-      <PageSwitcher banner={banner}>
-        <ElementSearch
+      <motion.div
+        initial={{ scale: .97, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ ease: 'easeOut', duration: .1 }}
+        className="flex gap-10 p-10 overflow-hidden h-full"
+      >
+        <AnnotatedScreenshot
           deviceScreen={deviceScreen}
           onElementHovered={setHoveredElement}
           hoveredElement={hoveredElement}
           onElementSelected={setSelectedElement}
+          selectedElement={selectedElement}
         />
-        {detailsPage}
-      </PageSwitcher>
+        <PageSwitcher banner={banner}>
+          <ElementSearch
+            deviceScreen={deviceScreen}
+            onElementHovered={setHoveredElement}
+            hoveredElement={hoveredElement}
+            onElementSelected={setSelectedElement}
+          />
+          {detailsPage}
+        </PageSwitcher>
+      </motion.div>
+      <Footer
+        selectedElement={selectedElement}
+        hoveredElement={hoveredElement}
+      />
     </motion.div>
   )
 }
