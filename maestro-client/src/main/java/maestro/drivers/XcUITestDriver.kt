938a329b20fe8a7125c2b7439e273466e0b437b4
Amanjeet Singh <amanjeetsingh150@gmail.com>
2022-12-28 21:51:21 +0530

    Close the response when validating server up
    - Optimize imports
    - Print the count of retry

--- maestro-client/src/main/java/maestro/drivers/XcUITestDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/XcUITestDriver.kt
@@ -1,22 +1,16 @@
 package maestro.drivers
 
-import ios.xcrun.CommandLineUtils
 import ios.xcrun.Simctl
 import maestro.Maestro
 import maestro.logger.Logger
 import maestro.utils.MaestroTimer
-import okhttp3.OkHttp
-import okhttp3.OkHttpClient
-import okhttp3.Request
 import okio.buffer
 import okio.sink
 import okio.source
 import org.rauschig.jarchivelib.ArchiverFactory
+import xcuitest.XCTestDriverClient
 import java.io.File
-import java.io.IOException
 import java.net.ConnectException
-import java.net.Socket
-import java.util.concurrent.CompletableFuture
 
 class XcUITestDriver(private val logger: Logger, private val deviceId: String) {
 
@@ -36,7 +30,7 @@ class XcUITestDriver(private val logger: Logger, private val deviceId: String) {
                 return
             } else {
                 logger.info("[Failed] Ensuring ui test runner app is launched on $deviceId")
-                logger.info("[Retry] Retrying setup()")
+                logger.info("[Retry] Retrying setup() ${i}th time")
             }
         }
     }
@@ -45,13 +39,7 @@ class XcUITestDriver(private val logger: Logger, private val deviceId: String) {
         Simctl.ensureAppAlive(UI_TEST_RUNNER_APP_BUNDLE_ID)
         return MaestroTimer.retryUntilTrue(10_000, 100) {
             try {
-                val client = OkHttpClient()
-                val call = client.newCall(Request.Builder()
-                    .url("http://localhost:9080/subTree/?appId=com.apple.springboard")
-                    .get()
-                    .build())
-                val response = call.execute()
-                response.isSuccessful
+                XCTestDriverClient.subTree(UI_TEST_RUNNER_APP_BUNDLE_ID).use { it.isSuccessful }
             } catch (ignore: ConnectException) {
                 false
             }
