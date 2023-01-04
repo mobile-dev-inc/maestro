72aca373df039704163cf8adf63ba3ace7399700
jrexd <66198401+jrexd@users.noreply.github.com>
2023-01-04 05:40:08 -0700

    Tweak Swipe Command Start/End Points (#519)
    * swipe tweaks
    
    * Update AndroidDriver.kt
    
    * Update AndroidDriver.kt
    
    * use heightGrid and widthGrid, fix start point calc
    
    * stay consistent between platforms
    
    * correct start/end points
    
    Co-authored-by: Josh Davis <joshd@simplenexus.com>

--- maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
@@ -271,26 +271,26 @@ class AndroidDriver(
         val deviceInfo = deviceInfo()
         when (swipeDirection) {
             SwipeDirection.UP -> {
-                val startX = deviceInfo.widthPixels / 2
-                val startY = deviceInfo.heightPixels
-                val endY = deviceInfo.heightPixels / 2
+                val startX = deviceInfo.widthGrid / 2
+                val startY = deviceInfo.heightGrid / 3
+                val endY = deviceInfo.heightGrid / 9
                 dadb.shell("input swipe $startX $startY $startX $endY $durationMs")
             }
             SwipeDirection.DOWN -> {
-                val startX = deviceInfo.widthPixels / 2
-                val startY = 0
-                val endY = deviceInfo.heightPixels / 2
+                val startX = deviceInfo.widthGrid / 2
+                val startY = deviceInfo.heightGrid / 9
+                val endY = deviceInfo.heightGrid / 3
                 dadb.shell("input swipe $startX $startY $startX $endY $durationMs")
             }
             SwipeDirection.RIGHT -> {
-                val startX = deviceInfo.widthPixels / 2
-                val startY = deviceInfo.heightPixels / 2
-                val endX = deviceInfo.widthPixels
+                val startX = deviceInfo.widthGrid / 2
+                val startY = deviceInfo.heightGrid / 2
+                val endX = deviceInfo.widthGrid
                 dadb.shell("input swipe $startX $startY $endX $startY $durationMs")
             }
             SwipeDirection.LEFT -> {
-                val startX = deviceInfo.widthPixels / 2
-                val startY = deviceInfo.heightPixels / 2
+                val startX = deviceInfo.widthGrid / 2
+                val startY = deviceInfo.heightGrid / 2
                 val endX = 0
                 dadb.shell("input swipe $startX $startY $endX $startY $durationMs")
             }
