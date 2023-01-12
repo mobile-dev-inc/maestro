b0da5d8d76289fc1275b513e40c4205d34c5b05a
Mahdi Sadeghi <sadeghi.mahdi@gmail.com>
2023-01-12 10:34:55 +0000

    Fix openLink command to support extras (#589)
    fix openLink command to support extras (example://deeplink?a=100&b=200)
    
    Co-authored-by: Mahdi Sadeghi <mahdi.sadeghi@skyscanner.net>

--- maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
@@ -400,7 +400,7 @@ class AndroidDriver(
     }
 
     override fun openLink(link: String) {
-        dadb.shell("am start -d $link")
+        dadb.shell("am start -d \"$link\"")
     }
 
     override fun setLocation(latitude: Double, longitude: Double) {
