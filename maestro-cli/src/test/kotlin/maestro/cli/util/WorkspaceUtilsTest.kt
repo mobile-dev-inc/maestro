74951efded5b988a8ce88142eb066179fd858fec
Dima Zaytsev <dmitry.zaicew@gmail.com>
2023-01-12 15:46:03 +0100

    Fix: maestro upload was ignoring js files

--- maestro-cli/src/test/kotlin/maestro/cli/util/WorkspaceUtilsTest.kt
+++ maestro-cli/src/test/kotlin/maestro/cli/util/WorkspaceUtilsTest.kt
@@ -1,6 +1,6 @@
 package maestro.cli.util
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import org.apache.commons.io.FileUtils
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
@@ -10,53 +10,73 @@ class WorkspaceUtilsTest {
     private fun flowFiles(): List<Path> {
         val flowA = FileUtils.toFile(this.javaClass.getResource("/tags/flowA.yaml"))
         val flowB = FileUtils.toFile(this.javaClass.getResource("/tags/flowB.yaml"))
-        return listOf(flowA.toPath(), flowB.toPath())
+        val script = FileUtils.toFile(this.javaClass.getResource("/tags/script.js"))
+        return listOf(flowA.toPath(), flowB.toPath(), script.toPath())
     }
 
     @Test
     fun `Test filterFlowsFilesBasedOnTags only include-tags with one option`() {
+        // When
         val result = WorkspaceUtils.filterFlowFilesBasedOnTags(
             flowFiles(),
             includeTags = listOf("dev"),
         )
 
-        Truth.assertThat(result).hasSize(2)
-        Truth.assertThat(result.first().fileName.toString()).isEqualTo("flowA.yaml")
-        Truth.assertThat(result[1].fileName.toString()).isEqualTo("flowB.yaml")
+        // Then
+        assertThat(result.filenames()).containsExactly(
+            "flowA.yaml",
+            "flowB.yaml",
+            "script.js",
+        )
     }
 
     @Test
     fun `Test filterFlowsFilesBasedOnTags only include-tags, but with multiple options`() {
+        // When
         val result = WorkspaceUtils.filterFlowFilesBasedOnTags(
             flowFiles(),
             includeTags = listOf("dev", "pull-request"),
         )
 
-        Truth.assertThat(result).hasSize(2)
-        Truth.assertThat(result.first().fileName.toString()).isEqualTo("flowA.yaml")
-        Truth.assertThat(result[1].fileName.toString()).isEqualTo("flowB.yaml")
+        // Then
+        assertThat(result.filenames()).containsExactly(
+            "flowA.yaml",
+            "flowB.yaml",
+            "script.js",
+        )
     }
 
     @Test
     fun `Test filterFlowsFilesBasedOnTags only exclude-tags`() {
+        // When
         val result = WorkspaceUtils.filterFlowFilesBasedOnTags(
             flowFiles(),
             excludeTags = listOf("pull-request"),
         )
 
-        Truth.assertThat(result).hasSize(1)
-        Truth.assertThat(result.first().fileName.toString()).isEqualTo("flowB.yaml")
+        // Then
+        assertThat(result.filenames()).containsExactly(
+            "flowB.yaml",
+            "script.js",
+        )
     }
 
     @Test
     fun `Test filterFlowsFilesBasedOnTags proving both`() {
+        // When
         val result = WorkspaceUtils.filterFlowFilesBasedOnTags(
             flowFiles(),
             includeTags = listOf("dev"),
             excludeTags = listOf("pull-request"),
         )
 
-        Truth.assertThat(result).hasSize(1)
-        Truth.assertThat(result.first().fileName.toString()).isEqualTo("flowB.yaml")
+        // Then
+        assertThat(result.filenames()).containsExactly(
+            "flowB.yaml",
+            "script.js",
+        )
     }
+
+    private fun List<Path>.filenames() = this.map { it.fileName.toString() }
+
 }
