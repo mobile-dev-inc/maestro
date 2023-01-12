ed5ae4e9bfc19890538fb8640af155f5dbd74b90
Dmitry Zaytsev <dmitry.zaicew@gmail.com>
2023-01-12 19:14:02 +0100

    Fix: include subflows regardless of their tags (#601)

--- maestro-cli/src/test/kotlin/maestro/cli/util/WorkspaceUtilsTest.kt
+++ maestro-cli/src/test/kotlin/maestro/cli/util/WorkspaceUtilsTest.kt
@@ -10,8 +10,16 @@ class WorkspaceUtilsTest {
     private fun flowFiles(): List<Path> {
         val flowA = FileUtils.toFile(this.javaClass.getResource("/tags/flowA.yaml"))
         val flowB = FileUtils.toFile(this.javaClass.getResource("/tags/flowB.yaml"))
+        val subFlow = FileUtils.toFile(this.javaClass.getResource("/tags/subflow.yaml"))
+        val subSubFlow = FileUtils.toFile(this.javaClass.getResource("/tags/subsubflow.yaml"))
         val script = FileUtils.toFile(this.javaClass.getResource("/tags/script.js"))
-        return listOf(flowA.toPath(), flowB.toPath(), script.toPath())
+        return listOf(
+            flowA.toPath(),
+            flowB.toPath(),
+            subFlow.toPath(),
+            subSubFlow.toPath(),
+            script.toPath()
+        )
     }
 
     @Test
@@ -26,6 +34,8 @@ class WorkspaceUtilsTest {
         assertThat(result.filenames()).containsExactly(
             "flowA.yaml",
             "flowB.yaml",
+            "subflow.yaml",
+            "subsubflow.yaml",
             "script.js",
         )
     }
@@ -42,6 +52,8 @@ class WorkspaceUtilsTest {
         assertThat(result.filenames()).containsExactly(
             "flowA.yaml",
             "flowB.yaml",
+            "subflow.yaml",
+            "subsubflow.yaml",
             "script.js",
         )
     }
@@ -57,6 +69,8 @@ class WorkspaceUtilsTest {
         // Then
         assertThat(result.filenames()).containsExactly(
             "flowB.yaml",
+            "subflow.yaml",
+            "subsubflow.yaml",
             "script.js",
         )
     }
@@ -73,6 +87,8 @@ class WorkspaceUtilsTest {
         // Then
         assertThat(result.filenames()).containsExactly(
             "flowB.yaml",
+            "subflow.yaml",
+            "subsubflow.yaml",
             "script.js",
         )
     }
