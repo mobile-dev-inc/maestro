32d3408872654553bda3c056f99c5d1bcc2e3eb7
Arthur Saveliev <arthur.saveliev@gmail.com>
2023-01-19 19:13:05 +0400

    Makes scroll direction case insensitive

--- maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlScrollUntilVisible.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlScrollUntilVisible.kt
@@ -1,9 +1,11 @@
 package maestro.orchestra.yaml
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import maestro.ScrollDirection
 
 data class YamlScrollUntilVisible(
+    @JsonFormat(with = [JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES])
     val direction: ScrollDirection ?= null,
-    val element: YamlElementSelectorUnion? = null,
+    val element: YamlElementSelectorUnion,
     val timeout: Long? = null,
 )
