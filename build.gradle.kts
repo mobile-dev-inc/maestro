4b37f0d7900f750c9840a1d56a6138f8477f4d75
Dima Zaytsev <dmitry.zaicew@gmail.com>
2023-01-30 14:12:36 +0100

    Reverted Android plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("com.android.tools.build:gradle:4.2.1")
     }
 }
 
