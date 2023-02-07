9d1890ea2da919d6ec4da041d502e84adf18689c
Axel Niklasson <axel@mobile.dev>
2023-02-07 11:24:26 +0100

    Remove unintended change

--- maestro-cli/src/main/java/maestro/cli/command/mockserver/sampleworkspace/index.js
+++
@@ -1,7 +0,0 @@
-get('/posts', (req, res, session) => {
-    session.count = (session.count || 0) + 1;
-
-    res.json({
-        posts: [1, 2, 3].map(i => ({ id: i, title: `Post $\{i\}` }))
-    });
-});
