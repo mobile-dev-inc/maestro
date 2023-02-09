baea7b527adacb86c142c78fb5c38be0fb9be282
berik <berik@mobile.dev>
2023-02-09 12:54:33 +0100

    Solve an issue where Element selector crashes the XCTestRunner

--- maestro-driver-ios/maestro-driver-iosUITests/Routes/Handlers/InputTextRouteHandler.swift
+++ maestro-driver-ios/maestro-driver-iosUITests/Routes/Handlers/InputTextRouteHandler.swift
@@ -3,12 +3,8 @@ import XCTest
 import os
 
 class InputTextRouteHandler : RouteHandler {
-    private enum Constants {
-        // 15 characters per second
-        static let typingFrequency = 15
-        static let maxTextLength = 45
-    }
-    
+    static let typingFrequency = 15
+
     private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "InputTextRouteHandler")
     
     func handle(request: FlyingFox.HTTPRequest) async throws -> FlyingFox.HTTPResponse {
@@ -18,13 +14,9 @@ class InputTextRouteHandler : RouteHandler {
             return errorResponse(message: "incorrect request body provided")
         }
         
-        if (requestBody.text.count > Constants.maxTextLength) {
-            return fallbackOnCopyPaste(text: requestBody.text, request: request)
-        }
-        
         do {
             let start = Date()
-            try await RunnerDaemonProxy().send(string: requestBody.text, typingFrequency: Constants.typingFrequency)
+            try await RunnerDaemonProxy().send(string: requestBody.text, typingFrequency: typingFrequency)
             let duration = Date().timeIntervalSince(start)
             logger.info("Text input duration took \(duration)")
             return HTTPResponse(statusCode: .ok)
@@ -33,28 +25,7 @@ class InputTextRouteHandler : RouteHandler {
             return errorResponse(message: "internal error")
         }
     }
-    
-    private func fallbackOnCopyPaste(text: String, request: FlyingFox.HTTPRequest) -> FlyingFox.HTTPResponse {
-        guard let appId = request.query["appId"] else {
-            logger.error("Requested view hierarchy for an invalid appId")
-            return HTTPResponse(statusCode: HTTPStatusCode.badRequest)
-        }
-        
-        let xcuiApplication = XCUIApplication(bundleIdentifier: appId)
-        
-        let element = xcuiApplication
-            .descendants(matching: .any)
-            .element(matching: NSPredicate(format: "hasKeyboardFocus == true"))
-        
-        if (!element.exists) {
-            return HTTPResponse(statusCode: .notFound)
-        }
-        
-        element.setText(text: text, application: xcuiApplication)
-        
-        return HTTPResponse(statusCode: .ok)
-    }
-    
+
     private func errorResponse(message: String) -> HTTPResponse {
         logger.error("Failed to input text - \(message)")
         let jsonString = """
