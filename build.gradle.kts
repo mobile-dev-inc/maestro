094f3b1167ce658be376060c5d9e51e353e3e79f
Nils Druyen <nils.druyen@fressnapf.com>
2023-02-17 00:00:42 +0100

    Retrieve plugin versions from version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.vanniktech.publish)
-    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.protobuf) apply false
+    alias(libs.plugins.vanniktech.publish)
     alias(libs.plugins.detekt)
     alias(libs.plugins.gradleVersions)
 }
