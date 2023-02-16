378ff014d3de539843556d12439d97f808b24a7b
Nils Druyen <nils.druyen@fressnapf.com>
2023-02-16 21:26:24 +0100

    Replace all dependencies with version catalog reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 buildscript {
     repositories {
         google()
@@ -9,10 +11,12 @@ buildscript {
     }
 }
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.vanniktech.maven.publish") version "0.19.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    alias(libs.plugins.gradleVersions)
 }
 
 repositories {
@@ -36,5 +40,19 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+// https://github.com/ben-manes/gradle-versions-plugin
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
