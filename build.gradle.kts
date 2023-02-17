72f4935e224c54a2787abd624db8191426468509
Nils Druyen <nils.druyen@fressnapf.com>
2023-02-17 09:26:25 +0100

    Remove ben banes versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -7,7 +5,6 @@ plugins {
     alias(libs.plugins.google.protobuf) apply false
     alias(libs.plugins.vanniktech.publish)
     alias(libs.plugins.detekt)
-    alias(libs.plugins.gradleVersions)
 }
 
 java {
@@ -20,18 +17,4 @@ detekt {
     allRules = false
     autoCorrect = true
     config = files("${rootDir}/detekt.yml")
-}
-
-// https://github.com/ben-manes/gradle-versions-plugin
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
 }
