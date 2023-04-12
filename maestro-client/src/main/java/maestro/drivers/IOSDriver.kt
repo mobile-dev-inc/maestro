51a36943714e8a92814194e45c9a7f0c647f1c86
Felipe Volpone <felipe@mobile.dev>
2023-04-12 15:57:13 -0300

    Adding the sessionId to the launchApp command so it can set it once the session starts

--- maestro-client/src/main/java/maestro/drivers/IOSDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/IOSDriver.kt
@@ -53,6 +53,7 @@ import java.nio.file.Files
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
+import java.util.UUID
 import kotlin.collections.set
 
 class IOSDriver(
@@ -103,6 +104,7 @@ class IOSDriver(
     override fun launchApp(
         appId: String,
         launchArguments: List<String>,
+        sessionId: UUID?,
     ) {
         iosDevice.launch(appId, launchArguments)
             .onSuccess { this.appId = appId }
