f69743550bc8fec00fabad63eb26d165031b1f53
artem888 <artem.sidorenko8@gmail.com>
2023-04-20 18:02:36 +0200

    rename swizzled function and add a comment, tnx @berikv

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/maestro_driver_iosUITests.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/maestro_driver_iosUITests.swift
@@ -5,9 +5,11 @@ class maestro_driver_iosUITests: XCTestCase {
     private static var swizzledOutIdle = false
 
     override func setUpWithError() throws {
+        // Disable waiting for quiescence
         if !maestro_driver_iosUITests.swizzledOutIdle { // ensure the swizzle only happens once
             let original = class_getInstanceMethod(objc_getClass("XCUIApplicationProcess") as? AnyClass, Selector(("waitForQuiescenceIncludingAnimationsIdle:")))
-            let replaced = class_getInstanceMethod(type(of: self), #selector(maestro_driver_iosUITests.replace))
+            let replaced = class_getInstanceMethod(type(of: self),
+                                                   #selector(maestro_driver_iosUITests.replace_waitForQuiescenceIncludingAnimationsIdle))
             
             guard let original = original, let replaced = replaced else { return }
             
@@ -21,7 +23,7 @@ class maestro_driver_iosUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
-    @objc func replace() {
+    @objc func replace_waitForQuiescenceIncludingAnimationsIdle() {
         return
     }
     
