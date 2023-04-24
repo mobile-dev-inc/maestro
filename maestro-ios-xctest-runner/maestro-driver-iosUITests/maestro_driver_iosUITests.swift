7174c1875775e4004e51cdcd37be8d83f3bf5592
artem888 <artem.sidorenko8@gmail.com>
2023-04-24 12:03:32 +0200

    [iOS] Resolve swipe flakiness (#1008)
    * swizzle waitForQuiescenceIncludingAnimationsIdle method to avoid wait for app to idle issues
    
    * rename swizzled function and add a comment, tnx @berikv
    
    * Build xtest runner binaries
    
    ---------
    
    Co-authored-by: berik <berik@mobile.dev>

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/maestro_driver_iosUITests.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/maestro_driver_iosUITests.swift
@@ -2,20 +2,31 @@ import XCTest
 import FlyingFox
 
 class maestro_driver_iosUITests: XCTestCase {
+    private static var swizzledOutIdle = false
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Disable waiting for quiescence
+        if !maestro_driver_iosUITests.swizzledOutIdle { // ensure the swizzle only happens once
+            let original = class_getInstanceMethod(objc_getClass("XCUIApplicationProcess") as? AnyClass, Selector(("waitForQuiescenceIncludingAnimationsIdle:")))
+            let replaced = class_getInstanceMethod(type(of: self),
+                                                   #selector(maestro_driver_iosUITests.replace_waitForQuiescenceIncludingAnimationsIdle))
+            
+            guard let original = original, let replaced = replaced else { return }
+            
+            method_exchangeImplementations(original, replaced)
+            maestro_driver_iosUITests.swizzledOutIdle = true
+        }
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    @objc func replace_waitForQuiescenceIncludingAnimationsIdle() {
+        return
     }
-
+    
     func testHttpServer() async throws {
         let server = XCTestHTTPServer()
         try await server.start()
