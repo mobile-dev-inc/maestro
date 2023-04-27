1f6697049375ca5b9b5bbf925ebce1faebfcc8d4
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-04-27 15:54:27 +0530

    Hide keyboard only dispatch back event (#1023)

--- maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/AndroidDriver.kt
@@ -361,7 +361,6 @@ class AndroidDriver(
 
     override fun hideKeyboard() {
         dadb.shell("input keyevent 4") // 'Back', which dismisses the keyboard before handing over to navigation
-        dadb.shell("input keyevent 111") // 'Escape'
         Thread.sleep(300)
         waitForAppToSettle(null, null)
     }
