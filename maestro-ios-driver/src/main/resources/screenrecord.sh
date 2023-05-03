a415740ef20775c301ac81ea9e968497af16f8e7
artem888 <artem.sidorenko8@gmail.com>
2023-05-03 19:39:52 +0200

    revert removing comments

--- maestro-ios-driver/src/main/resources/screenrecord.sh
+++ maestro-ios-driver/src/main/resources/screenrecord.sh
@@ -1,3 +1,15 @@
+# The `simctl recordVideo` command requires a SIGINT to be sent to stop the recording.
+# Before the SIGINT is sent, the video file is not playable.
+# Kotlin / JVM has no API to sent signals to subprocesses.
+# To work around that one could try to use `kill -SIGINT $pid`.
+# Kotlin / JVM on language level < 9 has no API to get the PID of a subprocess.
+# There just isn't a good way to make Kotlin record a video using xctest simctl.
+# That's where this script comes in. It send the SIGINT to simctl as soon as its
+# STDIN pipe is closed.
+
+# Also not that the backend currently does not support hvec. That is why the
+# codec is set to h264.
+
 echo "Start recording"
 xcrun simctl io "$DEVICE_ID" recordVideo --force --codec h264 "$RECORDING_PATH" &
 simctlpid=$!
