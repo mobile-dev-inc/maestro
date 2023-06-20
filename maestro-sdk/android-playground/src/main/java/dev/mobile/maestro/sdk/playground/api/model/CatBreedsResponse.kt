92807da6b543415855e65506ff825064131103ad
Axel Niklasson <axel@mobile.dev>
2023-06-20 09:14:48 +0200

    Remove deprecated SDKs

--- maestro-sdk/android-playground/src/main/java/dev/mobile/maestro/sdk/playground/api/model/CatBreedsResponse.kt
+++
@@ -1,5 +0,0 @@
-package dev.mobile.maestro.sdk.playground.api.model
-
-data class CatBreedsResponse(
-    val data: List<CatBreed>,
-)
--- maestro-sdk/rn/scripts/publish.sh
+++
@@ -1,4 +0,0 @@
-VERSION_NAME=$(grep VERSION_NAME gradle.properties | cut -d'=' -f2)
-sed -i '' "s/{VERSION}/$VERSION_NAME/g" maestro-sdk/rn/package.json
-cd maestro-sdk/rn
-npm publish
