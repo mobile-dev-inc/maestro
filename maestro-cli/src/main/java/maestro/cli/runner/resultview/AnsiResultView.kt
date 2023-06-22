e0e9cc6ff947c598226d4f711b5eaec7840c6cdb
berik <berik@mobile.dev>
2023-06-22 13:14:33 +0200

    Remove more extra spaces in ansi output

--- maestro-cli/src/main/java/maestro/cli/runner/resultview/AnsiResultView.kt
+++ maestro-cli/src/main/java/maestro/cli/runner/resultview/AnsiResultView.kt
@@ -135,7 +135,7 @@ class AnsiResultView(
     private fun Ansi.printLogMessages(indent: Int, commandState: CommandState) {
         renderLineStart(indent + 1)
         render("   ")   // Space that a status symbol would normally occupy
-        render("@|yellow Log messages: |@\n")
+        render("@|yellow Log messages:|@\n")
 
         commandState.logMessages.forEach {
             renderLineStart(indent + 2)
