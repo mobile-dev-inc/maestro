2db10b6ca3c153aa51222b5e648644cb19ecf6bf
Proksh Luthra <prokshh@gmail.com>
2023-06-29 19:13:34 +0530

    added theme to localstoge initially

--- maestro-studio/web/src/components/common/theme.tsx
+++ maestro-studio/web/src/components/common/theme.tsx
@@ -9,9 +9,11 @@ const getDefaultTheme = (): Theme => {
     (!("theme" in localStorage) &&
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
+    localStorage.theme = "dark";
     document.documentElement.classList.add("dark");
     return "dark";
   } else {
+    localStorage.theme = "light";
     document.documentElement.classList.remove("dark");
     return "light";
   }
