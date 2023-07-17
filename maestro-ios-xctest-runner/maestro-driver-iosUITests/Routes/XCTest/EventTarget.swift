1568722b9be849bfda67577d47c1b78ab06d2664
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-07-17 17:55:33 +0530

    Dispatch swipe event with first any child of application

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/XCTest/EventTarget.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/XCTest/EventTarget.swift
@@ -4,10 +4,9 @@ import XCTest
 @MainActor
 struct EventTarget {
     let eventTarget: NSObject
-    init(bundleId: String?) {
-        let application = bundleId.map { XCUIApplication(bundleIdentifier: $0) } ?? XCUIApplication()
-
-        eventTarget = application
+    init(bundleId: String) {
+        let application = XCUIApplication(bundleIdentifier: bundleId)
+        eventTarget = application.children(matching: .any).firstMatch
             .perform(NSSelectorFromString("eventTarget"))
             .takeUnretainedValue() as! NSObject
     }
--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/XCTest/RunningApp.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/XCTest/RunningApp.swift
@@ -1,19 +1,22 @@
-//
-//  RunningApp.swift
-//  maestro-driver-iosUITests
-//
-//  Created by Amanjeet Singh on 17/07/23.
-//
-
 import Foundation
 import XCTest
+import os
 
 struct RunningApp {
     
     private static let springboardBundleId = "com.apple.springboard"
+    private static let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: Self.self)
+    )
     private init() {}
     
     static func getForegroundAppId(_ appIds: [String]) -> String {
+        if appIds.isEmpty {
+            logger.info("Empty installed apps found")
+            return ""
+        }
+        
         return appIds.first { appId in
             let app = XCUIApplication(bundleIdentifier: appId)
             
