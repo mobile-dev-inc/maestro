7fa26544a53ba6648d18d3ab7558359c640b7da7
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-07-24 18:44:54 +0530

    Raise warning about the max depth when it crosses 61

--- maestro-client/src/main/java/maestro/drivers/IOSDriver.kt
+++ maestro-client/src/main/java/maestro/drivers/IOSDriver.kt
@@ -44,9 +44,7 @@ import maestro.TreeNode
 import maestro.UiElement.Companion.toUiElement
 import maestro.UiElement.Companion.toUiElementOrNull
 import maestro.ViewHierarchy
-import maestro.utils.FileUtils
-import maestro.utils.MaestroTimer
-import maestro.utils.ScreenshotUtils
+import maestro.utils.*
 import okio.Sink
 import org.slf4j.LoggerFactory
 import util.XCRunnerCLIUtils
@@ -182,15 +180,20 @@ class IOSDriver(
     }
 
     override fun contentDescriptor(): TreeNode {
-        return when (val contentDescriptorResult = iosDevice.contentDescriptor()) {
-            is Ok -> mapHierarchy(contentDescriptorResult.value)
-            is Err -> TreeNode()
-        }
+        return viewHierarchy()
     }
 
     fun viewHierarchy(): TreeNode {
         val hierarchyResult = iosDevice.viewHierarchy().get()
         LOGGER.info("Depth of the screen is ${hierarchyResult?.depth ?: 0}")
+        if (hierarchyResult?.depth != null && hierarchyResult.depth > WARNING_MAX_DEPTH) {
+            Insights.clear()
+            val message = "The view hierarchy has been calculated. The current depth of the hierarchy " +
+                    "is ${hierarchyResult.depth}. If you are using React native, consider migrating to the new " +
+                    "architecture where view flattening is available. For more information on the " +
+                    "migration process, please visit: https://reactnative.dev/docs/new-architecture-intro"
+            Insights.report(Insight(message, Insight.Level.INSIGHT))
+        }
         val hierarchy = hierarchyResult?.axElement ?: return TreeNode()
         return mapViewHierarchy(hierarchy)
     }
@@ -558,6 +561,8 @@ class IOSDriver(
         private const val ELEMENT_TYPE_CHECKBOX = 12
         private const val ELEMENT_TYPE_SWITCH = 40
         private const val ELEMENT_TYPE_TOGGLE = 41
+
+        private const val WARNING_MAX_DEPTH = 61
 
         private val CHECKABLE_ELEMENTS = setOf(
             ELEMENT_TYPE_CHECKBOX,
