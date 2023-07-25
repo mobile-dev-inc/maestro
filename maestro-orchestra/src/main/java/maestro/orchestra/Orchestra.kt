2f760f02f347ec37d73c3082d8c404ffd6c5998a
Sandeep Joshi <sandeep.kmr@BLRETV-M900PVWL.local>
2023-07-25 19:28:49 +0530

    feat: return parentparentViewHierarchy.root in place of absolute root node in case of exception

--- maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
@@ -884,7 +884,7 @@ class Orchestra(
             filterFunc,
             parentViewHierarchy)?.hierarchy?: throw MaestroException.ElementNotFound(
             "Element not found: $description",
-            maestro.viewHierarchy().root,
+            parentViewHierarchy.root,
         )
     }
 
