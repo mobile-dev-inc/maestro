25b7d0b69620c47a9a66b55d837ed57f7995c81e
Leland Takamine <leland.takamine@gmail.com>
2023-07-28 14:13:30 -0700

    Add studio banner UI

--- maestro-studio/web/src/components/common/Header.tsx
+++ maestro-studio/web/src/components/common/Header.tsx
@@ -1,9 +1,27 @@
 import { ThemeToggle } from "../common/theme";
+import { API } from '../../api/api';
+import clsx from 'clsx';
+
+const HeaderBanner = () => {
+  const { data } = API.useBannerMessage({ refreshInterval: 1000 })
+  if (!data?.level || data.level === 'none') return null
+
+  const bgColor = { info: 'bg-blue-100', warning: 'bg-amber-100', error: 'bg-red-100' }[data.level]
+
+  return (
+    <div className={clsx("flex py-3 px-7", bgColor)}>
+      <span>{data.message}</span>
+    </div>
+  )
+}
 
 const Header = () => (
-  <div className="flex py-3 px-7 items-center bg-white dark:bg-slate-900 dark:text-white border-b border-slate-200 dark:border-slate-800">
-    <span className="font-bold cursor-default grow">$ maestro studio</span>
-    <ThemeToggle />
+  <div className="flex flex-col">
+    <div className="flex py-3 px-7 items-center bg-white dark:bg-slate-900 dark:text-white border-b border-slate-200 dark:border-slate-800">
+      <span className="font-bold cursor-default grow">$ maestro studio</span>
+      <ThemeToggle />
+    </div>
+    <HeaderBanner />
   </div>
 );
 
