93d0fb1e4b63a46af484d5b9c98a2cdf4948cf42
Leland Takamine <leland.takamine@gmail.com>
2023-08-21 13:49:27 -0700

    Error message improvements
    * Proper error messages for empty workspaces
    * Surface workspace validation errors before connecting to device when running `maestro test`
    * Lay test fixture groundwork for future error improvements
    
    Behavior Change:
    This PR also changes the behavior when includeTags are specified in both config.yaml and as a command line option. Previously, in order for a flow to be included, it would need to match one of the config.yaml includeTags AND one of the command line includeTags. Even though this might be useful for some specific scenarios, this behavior is confusing and hard to explain. Now, in order for a flow to be included, it needs to match only one includeTag regardless of where the includeTag was specified.

--- maestro-cli/src/main/java/maestro/cli/App.kt
+++ maestro-cli/src/main/java/maestro/cli/App.kt
@@ -19,29 +19,16 @@
 
 package maestro.cli
 
-import maestro.cli.command.BugReportCommand
-import maestro.cli.command.CloudCommand
-import maestro.cli.command.DownloadSamplesCommand
-import maestro.cli.command.LoginCommand
-import maestro.cli.command.LogoutCommand
-import maestro.cli.command.PrintHierarchyCommand
-import maestro.cli.command.QueryCommand
-import maestro.cli.command.RecordCommand
-import maestro.cli.command.StudioCommand
-import maestro.cli.command.TestCommand
-import maestro.cli.command.UploadCommand
+import maestro.cli.command.*
 import maestro.cli.report.TestDebugReporter
 import maestro.cli.update.Updates
 import maestro.cli.util.ErrorReporter
 import maestro.cli.view.box
 import maestro.debuglog.DebugLogStore
-import maestro.debuglog.LogConfig
-import org.slf4j.LoggerFactory
 import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Option
-import java.util.Properties
-import kotlin.io.path.absolutePathString
+import java.util.*
 import kotlin.system.exitProcess
 
 @Command(
@@ -94,9 +81,6 @@ fun main(args: Array<String>) {
     // https://stackoverflow.com/a/17544259
     System.setProperty("apple.awt.UIElement", "true")
 
-    // logs & debug output
-    val logger = LoggerFactory.getLogger(App::class.java)
-
     Dependencies.install()
     Updates.fetchUpdatesAsync()
 
@@ -113,7 +97,6 @@ fun main(args: Array<String>) {
                 ex.stackTraceToString()
             }
 
-            logger.error(message)
             println()
             cmd.err.println(
                 cmd.colorScheme.errorText(message)
