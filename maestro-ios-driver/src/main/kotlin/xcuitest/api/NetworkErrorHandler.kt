c280967012904dc60fb2a8c49771f3e19bd2248d
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-08-21 21:05:21 +0530

    Fix creating connections (#1370)

--- maestro-ios-driver/src/main/kotlin/xcuitest/api/NetworkErrorHandler.kt
+++ maestro-ios-driver/src/main/kotlin/xcuitest/api/NetworkErrorHandler.kt
@@ -21,7 +21,7 @@ class NetworkErrorHandler(
     companion object {
         const val RETRY_RESPONSE_CODE = 503
         private const val NO_RETRY_RESPONSE_CODE = 502
-        private const val MAX_RETRY = 3
+        private const val MAX_RETRY = 5
         private val mapper = jacksonObjectMapper()
     }
 
