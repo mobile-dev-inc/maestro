48198a922299a91085382b23b090bd696a4caee5
Arthur Saveliev <arthur.saveliev@gmail.com>
2023-08-24 18:07:12 +0400

    Changes Cli error message color to red

--- maestro-cli/src/main/java/maestro/cli/App.kt
+++ maestro-cli/src/main/java/maestro/cli/App.kt
@@ -101,6 +101,12 @@ fun main(args: Array<String>) {
             }
 
             println()
+
+            // make errors red
+            cmd.colorScheme =  CommandLine.Help.ColorScheme.Builder()
+                .errors(CommandLine.Help.Ansi.Style.fg_red)
+                .build()
+
             cmd.err.println(
                 cmd.colorScheme.errorText(message)
             )
