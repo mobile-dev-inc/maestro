49b3e4b0089c5b3d9ca46baf8883f726482fcd16
Arthur Saveliev <arthur.saveliev@gmail.com>
2023-08-28 12:37:59 +0400

    fix message

--- maestro-cli/src/main/java/maestro/cli/device/DeviceCreateUtil.kt
+++ maestro-cli/src/main/java/maestro/cli/device/DeviceCreateUtil.kt
@@ -67,7 +67,7 @@ internal object DeviceCreateUtil {
             }
         }
 
-        if (existingDeviceId == null) PrintUtils.message("Created simulator with name $deviceName and udid $deviceUUID)")
+        if (existingDeviceId == null) PrintUtils.message("Created simulator with name $deviceName and UUID $deviceUUID)")
 
         return Device.AvailableForLaunch(
             modelId = deviceUUID,
