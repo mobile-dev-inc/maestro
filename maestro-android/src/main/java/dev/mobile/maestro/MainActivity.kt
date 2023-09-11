f2e586f989554db08a865c726462aed372932284
artem888 <artem.sidorenko8@gmail.com>
2023-09-11 19:42:38 +0200

    add MainActivity for maestro android app

---
+++ maestro-android/src/main/java/dev/mobile/maestro/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.mobile.maestro
+
+import android.os.Bundle
+import android.app.Activity
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
