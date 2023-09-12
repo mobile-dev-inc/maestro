242be928793933b8880a303714cbc3988cf9b984
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-09-12 12:44:11 +0530

    fix: adds try/catch in `ScreenDiffHandler.swift`

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/ScreenDiffHandler.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/ScreenDiffHandler.swift
@@ -12,31 +12,23 @@ struct IsScreenStaticHandler: HTTPHandler {
     )
     
     func handleRequest(_ request: FlyingFox.HTTPRequest) async throws -> FlyingFox.HTTPResponse {
-        let screenshot1 = XCUIScreen.main.screenshot()
-        let screenshot2 = XCUIScreen.main.screenshot()
-        let hash1 = SHA256.hash(data: screenshot1.pngRepresentation)
-        let hash2 = SHA256.hash(data: screenshot2.pngRepresentation)
-        
-        let isScreenStatic = hash1 == hash2
-        
-        let response = ["isScreenStatic" : isScreenStatic]
-        
-        guard let responseData = try? JSONSerialization.data(
-            withJSONObject: response,
-            options: .prettyPrinted
-        ) else {
-            let errorData = handleError(message: "serialization of isScreenStatic failed")
-            return HTTPResponse(statusCode: HTTPStatusCode.badRequest, body: errorData)
+        do {
+            let screenshot1 = XCUIScreen.main.screenshot()
+            let screenshot2 = XCUIScreen.main.screenshot()
+            let hash1 = SHA256.hash(data: screenshot1.pngRepresentation)
+            let hash2 = SHA256.hash(data: screenshot2.pngRepresentation)
+            
+            let isScreenStatic = hash1 == hash2
+            
+            let response = ["isScreenStatic" : isScreenStatic]
+            
+            let responseData = try JSONSerialization.data(
+                withJSONObject: response,
+                options: .prettyPrinted
+            )
+            return HTTPResponse(statusCode: .ok, body: responseData)
+        } catch let error {
+            return AppError(message: "Detecting screen static request failed. Error \(error.localizedDescription)").httpResponse
         }
-        
-        return HTTPResponse(statusCode: .ok, body: responseData)
-    }
-    
-    private func handleError(message: String) -> Data {
-        logger.error("Failed to check if screen is static - \(message)")
-        let jsonString = """
-             { "errorMessage" : \(message) }
-            """
-        return Data(jsonString.utf8)
     }
 }
