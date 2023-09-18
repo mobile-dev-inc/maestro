99e5277d1f553422187cd8b00036488cdab4e8de
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-09-18 13:57:55 +0530

    fix: deserialization in case of non-strings
    - for YamlInputText, YamlAssertTrue, YamlEvalScript

--- maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlInputText.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlInputText.kt
@@ -1,6 +1,7 @@
 package maestro.orchestra.yaml
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import java.lang.UnsupportedOperationException
 
 data class YamlInputText(
     val text: String,
@@ -11,8 +12,18 @@ data class YamlInputText(
 
         @JvmStatic
         @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
-        fun parse(text: String) = YamlInputText(
-            text = text,
-        )
+        fun parse(text: Any): YamlInputText {
+            val inputText = when (text) {
+                is String -> text
+                is Map<*, *> -> {
+                    val input = text.getOrDefault("text", "") as String
+                    val label = text.getOrDefault("label", "") as String
+                    return YamlInputText(input, label)
+                }
+                is Int, is Long, is Char, is Boolean, is Float, is Double -> text.toString()
+                else -> throw UnsupportedOperationException("Cannot deserialize input text with data type ${text.javaClass}")
+            }
+            return YamlInputText(text = inputText)
+        }
     }
 }
