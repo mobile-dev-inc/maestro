2dfe1f09a74070ab0c3f76d6882161861788072a
Marie Ramlow <marie.ramlow@bevuta.com>
2023-11-24 11:36:34 +0100

    Use gradle toolchains

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,9 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 detekt {
