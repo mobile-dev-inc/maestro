9e8e6bf7b2947858204335d922dbb468d4aa6485
fitriahfebriani <fitriahfebriani19@gmail.com>
2024-05-17 11:57:37 +0700

    add function to get test step

--- maestro-cli/src/main/java/maestro/cli/report/HtmlTestSuiteReporter.kt
+++ maestro-cli/src/main/java/maestro/cli/report/HtmlTestSuiteReporter.kt
@@ -16,10 +16,12 @@ class HtmlTestSuiteReporter : TestSuiteReporter {
         bufferedOut.close()
     }
 
-    private fun readLogFile(): Array<String> {
+    private fun getTestStep(): Array<Array<String>> {
         val debugOutputPath = TestDebugReporter.getDebugOutputPath()
         val filePathLog = "${debugOutputPath}/maestro.log"
         var reader: BufferedReader? = null
+        var testStep = emptyArray<String>()
+        var testSteps = emptyArray<Array<String>>()
         var failedStep = emptyArray<String>()
     
         try {
@@ -28,8 +30,14 @@ class HtmlTestSuiteReporter : TestSuiteReporter {
     
             while (reader.readLine().also { line = it } != null) {
                 // Process each line
-                if(line.contains("FAILED") && !line.contains("Run")){
-                    failedStep += line
+                if(line.contains("m.cli.runner.TestSuiteInteractor") && !line.contains("Run") && !line.contains("Define variables") && !line.contains("Apply configuration")){
+                    if(line.contains("COMPLETED") || line.contains("FAILED")){
+                        testStep += line
+                    }
+                }
+                if(line.contains("m.cli.runner.TestSuiteInteractor - Stop") && line.contains("COMPLETED")){
+                    testSteps += testStep
+                    testStep = emptyArray<String>()
                 }
             }
         } catch (e: Exception) {
@@ -40,12 +48,13 @@ class HtmlTestSuiteReporter : TestSuiteReporter {
                 println("An error occurred while closing the file: ${e.message}")
             }
         }
-        return failedStep
+        return testSteps
     }
 
     private fun buildHtmlReport(summary: TestExecutionSummary): String {
     val htmlBuilder = StringBuilder()
-    val failedStep = readLogFile()
+    // val failedStep = readLogFile()
+    val testSteps = getTestStep()
     var idx = 0
 
     htmlBuilder.append("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>Maestro Test Report</title><link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\"></head><body>")
@@ -62,11 +71,18 @@ class HtmlTestSuiteReporter : TestSuiteReporter {
 
             htmlBuilder.append("<p class=\"card-text\">Status: ${flow.status}<br>Duration: ${flow.duration}<br>File Name: ${flow.fileName}</p>")
             if (flow.failure != null) {
-                htmlBuilder.append("<p class=\"card-text text-danger\">${failedStep[idx]}</p>")
+                // htmlBuilder.append("<p class=\"card-text text-danger\">${failedStep[idx]}</p>")
                 htmlBuilder.append("<p class=\"card-text text-danger\">${flow.failure.message}</p>")
-                idx++
+                // idx++
             }
+            htmlBuilder.append("<div id=\"accordion\"><div class=\"card\"><div class=\"card-header\" id=\"headingOne\"><h5 class=\"mb-0\"><button class=\"btn btn-link\" data-toggle=\"collapse show\" data-target=\"#test-step\" aria-expanded=\"true\" aria-controls=\"test-step\">Detail Steps</button></h5></div>")
+            htmlBuilder.append("<div id=\"test-step\" class=\"collapse show\" aria-labelledby=\"headingOne\" data-parent=\"#accordion\"><div class=\"card-body\"><p class=\"card-text\">")
+            for(step in testSteps[idx]){
+                htmlBuilder.append("${step}<br>")
+            }
+            htmlBuilder.append("</p></div></div></div></div>")
             htmlBuilder.append("</div></div></div>")
+            idx++
         }
     }
     htmlBuilder.append("<script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js\"></script><script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js\"></script>")
