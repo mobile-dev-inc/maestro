1ed911b28c0cf32682a00f2f9d305292fe01c8a4
depapp <depa.article@gmail.com>
2024-05-17 09:52:55 +0700

    add missing div close tag

--- maestro-cli/src/main/java/maestro/cli/report/HtmlTestSuiteReporter.kt
+++ maestro-cli/src/main/java/maestro/cli/report/HtmlTestSuiteReporter.kt
@@ -70,7 +70,7 @@ class HtmlTestSuiteReporter : TestSuiteReporter {
         }
     }
     htmlBuilder.append("<script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js\"></script><script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js\"></script>")
-    htmlBuilder.append("</body></html>")
+    htmlBuilder.append("</div></div></div></body></html>")
     return htmlBuilder.toString()
     }
 }
