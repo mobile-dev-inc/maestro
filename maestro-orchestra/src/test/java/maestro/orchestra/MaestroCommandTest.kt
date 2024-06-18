9713a0e3df0f053c8a1bdb0a15dee65ce2668662
prasanta.biswas <prasanta.biswas@gojek.com>
2024-06-18 22:49:14 +0530

    added test for setLocation with negative coordinates

--- maestro-orchestra/src/test/java/maestro/orchestra/MaestroCommandTest.kt
+++ maestro-orchestra/src/test/java/maestro/orchestra/MaestroCommandTest.kt
@@ -42,4 +42,17 @@ internal class MaestroCommandTest {
         assertThat(description)
             .isEqualTo("Set Location to Test Laboratory")
     }
+
+    @Test
+    fun `description (negative coordinates)`() {
+        // given
+        val maestroCommand = MaestroCommand(SetLocationCommand("-12.5266", "-78.2150", "Set Location with negative coordinates"))
+
+        // when
+        val description = maestroCommand.description()
+
+        // then
+        assertThat(description)
+            .isEqualTo("Set Location with negative coordinates")
+    }
 }
