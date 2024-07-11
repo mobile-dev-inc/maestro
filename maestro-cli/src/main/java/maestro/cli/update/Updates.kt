de48ad29494545aa1e80d44a874b128a332b7672
Bartek Pacia <barpac02@gmail.com>
2024-07-11 19:26:54 +0100

    Updates: implement JAVA_VERSION getter

--- maestro-cli/src/main/java/maestro/cli/update/Updates.kt
+++ maestro-cli/src/main/java/maestro/cli/update/Updates.kt
@@ -16,6 +16,17 @@ import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 object Updates {
+    val JAVA_VERSION: Int
+        get() {
+            // Adapted from https://stackoverflow.com/a/2591122/7009800
+            val version = System.getProperty("java.version")
+            return if (version.startsWith("1.")) {
+                version.substring(2, 3).toInt()
+            } else {
+                val dot = version.indexOf(".")
+                if (dot != -1) version.substring(0, dot).toInt() else 0
+            }
+        }
 
     val OS_NAME: String = System.getProperty("os.name")
     val OS_ARCH: String = System.getProperty("os.arch")
