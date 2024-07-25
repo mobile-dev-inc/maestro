8be48d6bad4728237a36b73bdcba9c280799579f
Bartek Pacia <barpac02@gmail.com>
2024-07-26 00:10:55 +0200

    TouchRouteHandler: revert changes, prepare for new approach

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/TouchRouteHandler.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/TouchRouteHandler.swift
@@ -11,34 +11,21 @@ struct TouchRouteHandler: HTTPHandler {
     
     func handleRequest(_ request: FlyingFox.HTTPRequest) async throws -> FlyingFox.HTTPResponse {
         let decoder = JSONDecoder()
-      
-        let orientation = XCUIDevice.shared.orientation
         
-        logger.info("XCUIDevice.shared.orientation: \(String(describing: orientation))")
-      
         guard let requestBody = try? decoder.decode(TouchRequest.self, from: request.body) else {
             return AppError(type: .precondition, message: "incorrect request body provided for tap route").httpResponse
         }
         
-        let (width, height) = ScreenSizeHelper.physicalScreenSize()
-        
-        let (x, y) = switch (orientation) {
-        case .portrait: (requestBody.x, requestBody.y)
-        case .landscapeLeft: (width - requestBody.y, requestBody.x)
-        case .landscapeRight: (requestBody.y, height - requestBody.x)
-        default: fatalError("Not implemented yet")
-        }
-
         if requestBody.duration != nil {
-            logger.info("Long pressing \(x), \(y) for \(requestBody.duration!)s")
+            logger.info("Long pressing \(requestBody.x), \(requestBody.y) for \(requestBody.duration!)s")
         } else {
-            logger.info("Tapping \(x), \(y)")
+            logger.info("Tapping \(requestBody.x), \(requestBody.y)")
         }
 
         do {
             let eventRecord = EventRecord(orientation: .portrait)
             _ = eventRecord.addPointerTouchEvent(
-                at: CGPoint(x: CGFloat(x), y: CGFloat(y)),
+                at: CGPoint(x: CGFloat(requestBody.x), y: CGFloat(requestBody.y)),
                 touchUpAfter: requestBody.duration
             )
             let start = Date()
