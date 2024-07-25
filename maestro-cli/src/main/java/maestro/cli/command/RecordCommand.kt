881cefec83e0057932677ae82837216a2b1f2db6
Gabriel <gabriel.costache@mobilityhouse.com>
2024-07-25 11:48:56 +0200

    added `--config` parameter to RecordCommand

--- maestro-cli/src/main/java/maestro/cli/command/RecordCommand.kt
+++ maestro-cli/src/main/java/maestro/cli/command/RecordCommand.kt
@@ -56,6 +56,9 @@ class RecordCommand : Callable<Int> {
     @CommandLine.Parameters
     private lateinit var flowFile: File
 
+    @Option(names = ["--config"], description = ["Optional .yaml configuration file for Flows. If not provided, Maestro will look for a config.yaml file in the root directory."])
+    private var configFile: File? = null
+
     @Option(names = ["-e", "--env"])
     private var env: Map<String, String> = emptyMap()
 
@@ -80,6 +83,9 @@ class RecordCommand : Callable<Int> {
             throw CliError("--platform option was deprecated. You can remove it to run your test.")
         }
 
+        if (configFile != null && configFile?.exists()?.not() == true) {
+            throw CliError("The config file ${configFile?.absolutePath} does not exist.")
+        }
 
         TestDebugReporter.install(debugOutputPathAsString = debugOutput, printToConsole = parent?.verbose == true)
         val path = TestDebugReporter.getDebugOutputPath()
