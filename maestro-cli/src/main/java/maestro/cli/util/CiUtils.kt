36b9e785df7639e3c88e236c6071e5139394ff26
Bartek Pacia <barpac02@gmail.com>
2024-07-30 12:01:41 +0200

    CiUtils: add comments when particular CI service envvar was added

--- maestro-cli/src/main/java/maestro/cli/util/CiUtils.kt
+++ maestro-cli/src/main/java/maestro/cli/util/CiUtils.kt
@@ -1,18 +1,20 @@
 package maestro.cli.util
 
 object CiUtils {
+
+    // When adding a new CI, also add the first version of Maestro that supports it.
     private val ciEnvVarMap = mapOf(
-        "APPVEYOR" to "appveyor",
+        "APPVEYOR" to "appveyor", // since v1.37.4
         "BITBUCKET_BUILD_NUMBER" to "bitbucket",
         "BITRISE_IO" to "bitrise",
-        "BUILDKITE" to "buildkite",
+        "BUILDKITE" to "buildkite", // since v1.37.4
         "CIRCLECI" to "circleci",
-        "CIRRUS_CI" to "cirrusci",
-        "DRONE" to "drone",
+        "CIRRUS_CI" to "cirrusci", // since v1.37.4
+        "DRONE" to "drone", // since v1.37.4
         "GITHUB_ACTIONS" to "github",
         "GITLAB_CI" to "gitlab",
         "JENKINS_HOME" to "jenkins",
-        "TEAMCITY_VERSION" to "teamcity",
+        "TEAMCITY_VERSION" to "teamcity", // since v1.37.4
         "CI" to "ci"
     )
 
