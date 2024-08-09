630d3a111c02b32d975d3d74bd1d5780ee680db5
Bartek Pacia <barpac02@gmail.com>
2024-08-09 09:33:57 +0100

    apply resolver convention and use toolchains in almost all subprojects

--- maestro-cli/build.gradle.kts
+++ maestro-cli/build.gradle.kts
@@ -61,8 +61,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 tasks.create("createProperties") {
