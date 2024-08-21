e88635bc2f6c8244e7e0fbc0f2bdcb356d70b7c8
Bartek Pacia <barpac02@gmail.com>
2024-08-21 18:27:59 +0100

    improve Claude prompt

--- maestro-ai/src/main/java/maestro/ai/Prediction.kt
+++ maestro-ai/src/main/java/maestro/ai/Prediction.kt
@@ -3,6 +3,7 @@ package maestro.ai
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
+import maestro.ai.antrophic.Claude
 import maestro.ai.openai.OpenAI
 
 @Serializable
@@ -39,24 +40,65 @@ object Prediction {
         // |* You will make me sad if you raise report defects that are false positives.
         // |* Do not make up defects that are not present in the screenshot. It's fine if you don't find any defects.
 
-        val prompt = """
-            |You are a QA engineer performing quality assurance for a mobile application. Identify any defects in the provided screenshot.
-            |${if (assertion != null) "Additionally, you must ensure the following assertion is true: $assertion" else ""}
-            |
-            |RULES:
-            |* All defects you find must belong to one of the following categories:
-            |${categories.joinToString { "\n  * ${it.first}: ${it.second}" }}
-            | 
-            |* If you see defects, your response MUST only include defect name and detailed reasoning for each defect.
-            |* Provide response in the format: <defect name>:<reasoning>
-            |* Do not raise false positives. Some example responses that have a high chance of being a false positive:
-            |
-            |  * button is partially cropped at the bottom
-            |  * button is not aligned horizontally/vertically within its container
-            |
-            |${if (previousFalsePositives.isNotEmpty()) "Additionally, the following defects are false positives:" else ""}
-            |${if (previousFalsePositives.isNotEmpty()) previousFalsePositives.joinToString("\n") { "  * $it" } else ""}
-        """.trimMargin("|")
+        val prompt = buildString {
+
+            appendLine(
+                """
+                You are a QA engineer performing quality assurance for a mobile application.
+                Identify any defects in the provided screenshot.
+                """.trimIndent()
+            )
+
+            if (assertion != null) {
+                appendLine("Additionally, you must ensure the following assertion is true: $assertion")
+            }
+
+            append(
+                """
+                |RULES:
+                |* All defects you find must belong to one of the following categories:
+                |${categories.joinToString(separator = "\n") { "  * ${it.first}: ${it.second}" }}
+                |* If you see defects, your response MUST only include defect name and detailed reasoning for each defect.
+                |* Provide response as a list of JSON objects, each representing <category>:<reasoning>
+                |* Do not raise false positives. Some example responses that have a high chance of being a false positive:
+                |* button is partially cropped at the bottom
+                |* button is not aligned horizontally/vertically within its container
+                """.trimMargin("|")
+            )
+
+            // Claude doesn't have a JSON mode as of 21-08-2024
+            //  https://docs.anthropic.com/en/docs/test-and-evaluate/strengthen-guardrails/increase-consistency
+            if (aiClient is Claude) {
+                append(
+                    """
+                    |
+                    |* You must provide result as a valid JSON object, matching this structure:
+                    |
+                    |  {
+                    |      "defects": [
+                    |          {
+                    |              "category": "<defect category, string>",
+                    |              "reasoning": "<reasoning, string>"
+                    |          },
+                    |          {
+                    |              "category": "<defect category, string>",
+                    |              "reasoning": "<reasoning, string>"
+                    |          }
+                    |       ]
+                    |  }
+                    |
+                    |DO NOT output any other information in the JSON object.
+                    """.trimMargin("|")
+                )
+            }
+
+            if (previousFalsePositives.isNotEmpty()) {
+                appendLine("Additionally, the following defects are false positives:")
+                for (falsePositive in previousFalsePositives) {
+                    appendLine("  * $falsePositive")
+                }
+            }
+        }
 
         if (printPrompt) {
             println("--- PROMPT START ---")
