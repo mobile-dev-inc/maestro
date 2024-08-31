564bda9edb5e8c8d61af9bf3e6ccf27a20c9b8de
Muhammed Furkan Boran <furkan.boran@trendyol.com>
2024-08-31 23:36:07 +0300

    Fix test

--- maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlScrollUntilVisible.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlScrollUntilVisible.kt
@@ -9,7 +9,7 @@ data class YamlScrollUntilVisible(
     val direction: ScrollDirection = ScrollDirection.DOWN,
     val element: YamlElementSelectorUnion,
     val timeout: Long = ScrollUntilVisibleCommand.DEFAULT_TIMEOUT_IN_MILLIS,
-    val speed: String,
+    val speed: String = ScrollUntilVisibleCommand.DEFAULT_SCROLL_DURATION,
     val visibilityPercentage: Int = ScrollUntilVisibleCommand.DEFAULT_ELEMENT_VISIBILITY_PERCENTAGE,
     val centerElement: Boolean = ScrollUntilVisibleCommand.DEFAULT_CENTER_ELEMENT,
     val label: String? = null
