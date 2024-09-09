addac1e0d0d9c35083f7f2213be0d0b8356bc745
Bartek Pacia <barpac02@gmail.com>
2024-09-09 11:15:27 +0200

    default to `UIDeviceOrientation.portrait` if it is `UIDeviceOrientation.unknown`

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Helpers/ScreenSizeHelper.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Helpers/ScreenSizeHelper.swift
@@ -7,11 +7,22 @@ struct ScreenSizeHelper {
         let screenSize = springboardApp.frame.size
         return (Float(screenSize.width), Float(screenSize.height))
     }
-    
+
+    private static func actualOrientation() -> UIDeviceOrientation {
+        let orientation = XCUIDevice.shared.orientation
+        if orientation == .unknown {
+            // If orientation is "unknown", we assume it is "portrait" to
+            // work around https://stackoverflow.com/q/78932288/7009800
+            return UIDeviceOrientation.portrait
+        }
+
+        return orientation
+    }
+
     /// Takes device orientation into account.
     static func actualScreenSize() throws -> (Float, Float) {
-        let orientation = XCUIDevice.shared.orientation
-        
+        let orientation = actualOrientation()
+
         let (width, height) = physicalScreenSize()
         let (actualWidth, actualHeight) = switch (orientation) {
         case .portrait, .portraitUpsideDown: (width, height)
@@ -19,13 +30,13 @@ struct ScreenSizeHelper {
         case .faceDown, .faceUp, .unknown: throw AppError(message: "Unsupported orientation: \(orientation)")
         @unknown default: throw AppError(message: "Unsupported orientation: \(orientation)")
         }
-        
+
         return (actualWidth, actualHeight)
     }
-    
+
     static func orientationAwarePoint(width: Float, height: Float, point: CGPoint) -> CGPoint {
-        let orientation = XCUIDevice.shared.orientation
-        
+        let orientation = actualOrientation()
+
         return switch (orientation) {
         case .portrait: point
         case .landscapeLeft: CGPoint(x: CGFloat(width) - point.y, y: CGFloat(point.x))
