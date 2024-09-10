37634ec8f467a2acb63cc9c081face84b5842960
Bartek Pacia <barpac02@gmail.com>
2024-09-10 20:03:17 +0200

    LogConfig: make log format configurable

--- maestro-client/src/main/java/maestro/debuglog/LogConfig.kt
+++ maestro-client/src/main/java/maestro/debuglog/LogConfig.kt
@@ -9,34 +9,39 @@ import ch.qos.logback.core.status.NopStatusListener
 import org.slf4j.LoggerFactory
 
 object LogConfig {
-    private const val LOG_PATTERN = "%d{HH:mm:ss.SSS} [%-5level] %logger: %msg%n"
+    // TODO: Improve usefulness and flexibility of logging
+    //  [x] Add new log pattern for logs that go to stdout
+    //  [x] make patterns configurable from env var: MAESTRO_CLI_LOG_PATTERN_FILE, MAESTRO_CLI_LOG_PATTERN_CONSOLE
+    //  [ ] color console messages by the log level
+
+    // See https://logback.qos.ch/manual/layouts.html#method
+
+    private const val DEFAULT_FILE_LOG_PATTERN = "%d{HH:mm:ss.SSS} [%5level] %logger::%method %line: %msg%n"
+    private const val DEFAULT_CONSOLE_LOG_PATTERN = "%msg%n"
+
+    private val FILE_LOG_PATTERN: String = System.getenv("MAESTRO_CLI_LOG_PATTERN_FILE") ?: DEFAULT_FILE_LOG_PATTERN
+    private val CONSOLE_LOG_PATTERN: String = System.getenv("MAESTRO_CLI_LOG_PATTERN_CONSOLE") ?: DEFAULT_CONSOLE_LOG_PATTERN
 
     fun configure(logFileName: String, printToConsole: Boolean) {
         val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
         loggerContext.statusManager.add(NopStatusListener())
         loggerContext.reset()
 
-        val encoder = createEncoder(loggerContext)
-        createAndAddFileAppender(loggerContext, encoder, logFileName)
+        createAndAddFileAppender(loggerContext, logFileName)
         if (printToConsole) {
-            createAndAddConsoleAppender(loggerContext, encoder)
+            createAndAddConsoleAppender(loggerContext)
         }
 
         loggerContext.getLogger("ROOT").level = Level.INFO
     }
 
-    private fun createEncoder(loggerContext: LoggerContext): PatternLayoutEncoder {
-        return PatternLayoutEncoder().apply {
+    private fun createAndAddConsoleAppender(loggerContext: LoggerContext) {
+        val encoder = PatternLayoutEncoder().apply {
             context = loggerContext
-            pattern = LOG_PATTERN
+            pattern = CONSOLE_LOG_PATTERN
             start()
         }
-    }
 
-    private fun createAndAddConsoleAppender(
-        loggerContext: LoggerContext,
-        encoder: PatternLayoutEncoder,
-    ) {
         val consoleAppender = ch.qos.logback.core.ConsoleAppender<ILoggingEvent>().apply {
             context = loggerContext
             setEncoder(encoder)
@@ -46,11 +51,13 @@ object LogConfig {
         loggerContext.getLogger("ROOT").addAppender(consoleAppender)
     }
 
-    private fun createAndAddFileAppender(
-        loggerContext: LoggerContext,
-        encoder: PatternLayoutEncoder,
-        logFileName: String,
-    ) {
+    private fun createAndAddFileAppender(loggerContext: LoggerContext, logFileName: String) {
+        val encoder = PatternLayoutEncoder().apply {
+            context = loggerContext
+            pattern = FILE_LOG_PATTERN
+            start()
+        }
+
         val fileAppender = FileAppender<ILoggingEvent>().apply {
             context = loggerContext
             setEncoder(encoder)
