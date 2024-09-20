563785e8b6eed0612361d7ed4c71b68f4194dcc0
Max Phillips <max.phillips@nine.com.au>
2024-09-20 17:52:25 +1000

    update XCUIElement extension to compile on tvOS

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Extensions/XCUIElement+Extensions.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Extensions/XCUIElement+Extensions.swift
@@ -3,8 +3,11 @@ import XCTest
 
 extension XCUIElement {
     func setText(text: String, application: XCUIApplication) {
+        // TODO: Does this require a tvOS implementation?
+        #if !os(tvOS)
         UIPasteboard.general.string = text
         doubleTap()
         application.menuItems["Paste"].tap()
+        #endif
     }
 }
