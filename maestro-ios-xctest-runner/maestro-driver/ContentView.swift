c4dbed163c5d2563fa27979e15bcd6e28cae4f0b
Max Phillips <max.phillips@nine.com.au>
2024-09-20 18:57:35 +1000

    migrate maestro-driver to swiftui so tvOS and ios can share a target

---
+++ maestro-ios-xctest-runner/maestro-driver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  maestro-driver
+//
+//  Created by Max Phillips on 20/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ maestro-ios-xctest-runner/maestro-driver/maestro_driverApp.swift
@@ -0,0 +1,17 @@
+//
+//  maestro_driverApp.swift
+//  maestro-driver
+//
+//  Created by Max Phillips on 20/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct maestro_driverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
