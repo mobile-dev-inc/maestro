457e1a9080e8456672ab352a449852c9243f82b2
Max Phillips <max.phillips@nine.com.au>
2024-09-20 17:19:09 +1000

    create a new xcode target for tvOS

---
+++ maestro-ios-xctest-runner/maestro-driver-tvos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  maestro-driver-tvos
+//
+//  Created by Max Phillips on 20/9/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
