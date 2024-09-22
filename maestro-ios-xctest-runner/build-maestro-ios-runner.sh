d3bc0cbc8078a473b78baa5edc69ea42a936fd3b
Max Phillips <max.phillips@nine.com.au>
2024-09-22 16:34:17 +1000

    update xcode build script, remove old targets, run new ios / tvos build

--- maestro-ios-xctest-runner/build-maestro-ios-runner.sh
+++ maestro-ios-xctest-runner/build-maestro-ios-runner.sh
@@ -12,7 +12,7 @@ xcodebuild \
 	ARCHS="x86_64 arm64" \
 	ONLY_ACTIVE_ARCH=NO \
 	-project ./maestro-ios-xctest-runner/maestro-driver-ios.xcodeproj \
-	-scheme maestro-driver-ios \
+	-scheme maestro-driver \
 	-sdk iphonesimulator \
 	-destination "generic/platform=iOS Simulator" \
 	-IDEBuildLocationStyle=Custom \
@@ -20,19 +20,46 @@ xcodebuild \
 	-IDECustomBuildProductsPath="$PWD/build/Products" \
 	build-for-testing
 
+xcodebuild \
+	ARCHS="x86_64 arm64" \
+	ONLY_ACTIVE_ARCH=NO \
+	-project ./maestro-ios-xctest-runner/maestro-driver-ios.xcodeproj \
+	-scheme maestro-driver \
+	-sdk appletvsimulator \
+	-destination "generic/platform=tvOS Simulator" \
+	-IDEBuildLocationStyle=Custom \
+	-IDECustomBuildLocationType=Absolute \
+	-IDECustomBuildProductsPath="$PWD/build/Products" \
+	build-for-testing
+
 ## Remove intermediates, output and copy runner in maestro-ios-driver
 cp -r \
-	./build/Products/Debug-iphonesimulator/maestro-driver-iosUITests-Runner.app \
-	./maestro-ios-driver/src/main/resources/maestro-driver-iosUITests-Runner.app
+	./build/Products/Debug-iphonesimulator/maestro-driverUITests-Runner.app \
+	./maestro-ios-driver/src/main/resources/ios/maestro-driverUITests-Runner.app
 
 cp -r \
-	./build/Products/Debug-iphonesimulator/maestro-driver-ios.app \
-	./maestro-ios-driver/src/main/resources/maestro-driver-ios.app
+	./build/Products/Debug-appletvsimulator/maestro-driverUITests-Runner.app \
+	./maestro-ios-driver/src/main/resources/tvos/maestro-driverUITests-Runner.app
+
+cp -r \
+	./build/Products/Debug-iphonesimulator/maestro-driver.app \
+	./maestro-ios-driver/src/main/resources/ios/maestro-driver.app
+
+cp -r \
+	./build/Products/Debug-appletvsimulator/maestro-driver.app \
+	./maestro-ios-driver/src/main/resources/tvos/maestro-driver.app
 
 cp \
-	./build/Products/*.xctestrun \
-	./maestro-ios-driver/src/main/resources/maestro-driver-ios-config.xctestrun
+	./build/Products/*iphonesimulator*.xctestrun \
+	./maestro-ios-driver/src/main/resources/ios/maestro-driver-config.xctestrun
 
-(cd ./maestro-ios-driver/src/main/resources && zip -r maestro-driver-iosUITests-Runner.zip ./maestro-driver-iosUITests-Runner.app)
-(cd ./maestro-ios-driver/src/main/resources && zip -r maestro-driver-ios.zip ./maestro-driver-ios.app)
-rm -r ./maestro-ios-driver/src/main/resources/*.app
+cp \
+	./build/Products/*appletvsimulator*.xctestrun \
+	./maestro-ios-driver/src/main/resources/tvos/maestro-driver-config.xctestrun
+
+(cd ./maestro-ios-driver/src/main/resources/ios && zip -r maestro-driverUITests-Runner.zip ./maestro-driverUITests-Runner.app)
+(cd ./maestro-ios-driver/src/main/resources/tvos && zip -r maestro-driverUITests-Runner.zip ./maestro-driverUITests-Runner.app)
+(cd ./maestro-ios-driver/src/main/resources/ios && zip -r maestro-driver.zip ./maestro-driver.app)
+(cd ./maestro-ios-driver/src/main/resources/tvos && zip -r maestro-driver.zip ./maestro-driver.app)
+rm -r ./maestro-ios-driver/src/main/resources/ios/*.app
+rm -r ./maestro-ios-driver/src/main/resources/tvos/*.app
