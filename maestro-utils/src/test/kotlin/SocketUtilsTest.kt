2abc52b2acaa8a15ee8d66595b3ae2e4c5e829eb
Matheus <matheus_bereta@hotmail.com>
2024-10-11 11:01:09 -0300

    test: Refactoring the tests about localIp method

--- maestro-utils/src/test/kotlin/SocketUtilsTest.kt
+++ maestro-utils/src/test/kotlin/SocketUtilsTest.kt
@@ -1,6 +1,7 @@
+import io.mockk.every
+import io.mockk.mockkStatic
 import maestro.utils.SocketUtils
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -8,8 +9,11 @@ import org.junit.jupiter.api.Test
 import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.NetworkInterface
+import java.util.*
 
 class SocketUtilsTest {
+
+    private fun <T> List<T>.toEnumeration(): Enumeration<T> = Collections.enumeration(this)
 
     @Test
     fun `nextFreePort should return a free port within the specified range`() {
@@ -31,36 +35,21 @@ class SocketUtilsTest {
     }
 
     @Test
-    fun `localIp should return a non-loopback IPv4 address`() {
+    fun `localIp should return local IP address`() {
         val ip = SocketUtils.localIp()
 
         assertNotNull(ip)
         assertTrue(ip.startsWith("192") || ip.startsWith("10") || ip.startsWith("172") || ip.startsWith("127"))
-
-        val inetAddress = InetAddress.getByName(ip)
-
-        assertTrue(inetAddress is Inet4Address)
-        assertFalse(inetAddress.isLoopbackAddress)
+        assertTrue(InetAddress.getByName(ip) is Inet4Address)
     }
 
     @Test
     fun `localIp should return localhost address if no network interfaces are available`() {
-        val originalNetworkInterfaces = NetworkInterface::class.java.getDeclaredMethod("getNetworkInterfaces")
-        originalNetworkInterfaces.isAccessible = true
+        mockkStatic(NetworkInterface::class)
+        every { NetworkInterface.getNetworkInterfaces() } returns listOf<NetworkInterface>().toEnumeration()
 
-        try {
-            NetworkInterface::class.java.getDeclaredMethod("getNetworkInterfaces").apply {
-                isAccessible = true
-            }
+        val ip = SocketUtils.localIp()
 
-            val ip = SocketUtils.localIp()
-
-            assertNotNull(ip)
-            assertEquals(InetAddress.getLocalHost().hostAddress, ip)
-        } finally {
-            NetworkInterface::class.java.getDeclaredMethod("getNetworkInterfaces").apply {
-                isAccessible = true
-            }
-        }
+        assertEquals(InetAddress.getLocalHost().hostAddress, ip)
     }
 }
