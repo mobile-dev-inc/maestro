mobile-dev-inc/maestro

commit 290f5a69a514e62b49c36dc3ee8a58d30c5c7f42

Fix evaluate script bug (#2023)
* Fix evaluate script bug

* Fix test

---------

Co-authored-by: Muhammed Furkan Boran <furkan.boran@trendyol.com>

maestro-orchestra-models/src/main/java/maestro/orchestra/Commands.kt
@@ -101,7 +101,7 @@ data class ScrollUntilVisibleCommand(
     val direction: ScrollDirection,
     val scrollDuration: String = DEFAULT_SCROLL_DURATION,
     val visibilityPercentage: Int,
-    val timeout: Long = DEFAULT_TIMEOUT_IN_MILLIS,
+    val timeout: String = DEFAULT_TIMEOUT_IN_MILLIS,
     val centerElement: Boolean,
     override val label: String? = null,
     override val optional: Boolean = false,
@@ -113,6 +113,11 @@ data class ScrollUntilVisibleCommand(
         return ((1000 * (100 - this.toLong()).toDouble() / 100).toLong() + 1).toString()
     }
 
+    private fun String.timeoutToMillis(): String {
+        val timeout = if (this.toLong() < 0) { DEFAULT_TIMEOUT_IN_MILLIS.toLong() * 1000L } else this.toLong() * 1000L
+        return timeout.toString()
+    }
+
     override fun description(): String {
         return label ?: "Scrolling $direction until ${selector.description()} is visible."
     }
@@ -120,12 +125,13 @@ data class ScrollUntilVisibleCommand(
     override fun evaluateScripts(jsEngine: JsEngine): ScrollUntilVisibleCommand {
         return copy(
             selector = selector.evaluateScripts(jsEngine),
-            scrollDuration = scrollDuration.evaluateScripts(jsEngine).speedToDuration()
+            scrollDuration = scrollDuration.evaluateScripts(jsEngine).speedToDuration(),
+            timeout = timeout.evaluateScripts(jsEngine).timeoutToMillis(),
         )
     }
 
     companion object {
-        const val DEFAULT_TIMEOUT_IN_MILLIS = 20 * 1000L
+        const val DEFAULT_TIMEOUT_IN_MILLIS = "20"
         const val DEFAULT_SCROLL_DURATION = "40"
         const val DEFAULT_ELEMENT_VISIBILITY_PERCENTAGE = 100
         const val DEFAULT_CENTER_ELEMENT = false

maestro-orchestra/src/test/java/maestro/orchestra/yaml/YamlCommandReaderTest.kt
@@ -433,7 +433,7 @@ internal class YamlCommandReaderTest {
             ScrollUntilVisibleCommand(
                 selector = ElementSelector(textRegex = "Footer"),
                 direction = ScrollDirection.DOWN,
-                timeout = 20000,
+                timeout = "20",
                 scrollDuration = "40",
                 visibilityPercentage = 100,
                 label = "Scroll to the bottom",

maestro-test/src/test/resources/117_scroll_until_visible_speed.js
@@ -1,6 +1,11 @@
 output.speed = {
     slow: 40
 }
+
+output.timeout = {
+    slow: 20
+}
+
 output.element = {
     id: "maestro"
 }

maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
@@ -486,7 +486,7 @@ class Orchestra(
     }
 
     private fun scrollUntilVisible(command: ScrollUntilVisibleCommand): Boolean {
-        val endTime = System.currentTimeMillis() + command.timeout
+        val endTime = System.currentTimeMillis() + command.timeout.toLong()
         val direction = command.direction.toSwipeDirection()
         val deviceInfo = maestro.deviceInfo()
 

maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlFluentCommand.kt
@@ -614,17 +614,12 @@ data class YamlFluentCommand(
     }
 
     private fun scrollUntilVisibleCommand(yaml: YamlScrollUntilVisible): MaestroCommand {
-        val timeout =
-            if (yaml.timeout < 0) {
-                ScrollUntilVisibleCommand.DEFAULT_TIMEOUT_IN_MILLIS
-            } else yaml.timeout
-
         val visibility = if (yaml.visibilityPercentage < 0) 0 else if (yaml.visibilityPercentage > 100) 100 else yaml.visibilityPercentage
         return MaestroCommand(
             ScrollUntilVisibleCommand(
                 selector = toElementSelector(yaml.element),
                 direction = yaml.direction,
-                timeout = timeout,
+                timeout = yaml.timeout,
                 scrollDuration = yaml.speed,
                 visibilityPercentage = visibility,
                 centerElement = yaml.centerElement,

maestro-orchestra/src/main/java/maestro/orchestra/yaml/YamlScrollUntilVisible.kt
@@ -8,7 +8,7 @@ data class YamlScrollUntilVisible(
     @JsonFormat(with = [JsonFormat.Feature.ACCEPT_CASE_INSENSITIVE_PROPERTIES])
     val direction: ScrollDirection = ScrollDirection.DOWN,
     val element: YamlElementSelectorUnion,
-    val timeout: Long = ScrollUntilVisibleCommand.DEFAULT_TIMEOUT_IN_MILLIS,
+    val timeout: String = ScrollUntilVisibleCommand.DEFAULT_TIMEOUT_IN_MILLIS,
     val speed: String = ScrollUntilVisibleCommand.DEFAULT_SCROLL_DURATION,
     val visibilityPercentage: Int = ScrollUntilVisibleCommand.DEFAULT_ELEMENT_VISIBILITY_PERCENTAGE,
     val centerElement: Boolean = ScrollUntilVisibleCommand.DEFAULT_CENTER_ELEMENT,

maestro-test/src/test/kotlin/maestro/test/IntegrationTest.kt
@@ -3100,10 +3100,11 @@ class IntegrationTest {
     }
 
     @Test
-    fun `Case 117 - Scroll until view is visible - with speed evaluate`() {
+    fun `Case 117 - Scroll until view is visible - with speed and timeout evaluate`() {
         // Given
         val commands = readCommands("117_scroll_until_visible_speed")
         val expectedDuration = "601"
+        val expectedTimeout = "20000"
         val info = driver { }.deviceInfo()
 
         val elementBounds = Bounds(0, 0 + info.heightGrid, 100, 100 + info.heightGrid)
@@ -3116,14 +3117,17 @@ class IntegrationTest {
 
         // When
         var scrollDuration = "0"
+        var timeout = "0"
         Maestro(driver).use {
             orchestra(it, onCommandMetadataUpdate = { _, metaData ->
                 scrollDuration = metaData.evaluatedCommand?.scrollUntilVisible?.scrollDuration.toString()
+                timeout = metaData.evaluatedCommand?.scrollUntilVisible?.timeout.toString()
             }).runFlow(commands)
         }
 
         // Then
         assertThat(scrollDuration).isEqualTo(expectedDuration)
+        assertThat(timeout).isEqualTo(expectedTimeout)
         driver.assertEvents(
             listOf(
                 Event.SwipeElementWithDirection(Point(270, 480), SwipeDirection.UP, expectedDuration.toLong()),

maestro-test/src/test/resources/117_scroll_until_visible_speed.yaml
@@ -6,3 +6,4 @@ appId: com.example.app
       id: ${output.element.id}
     direction: DOWN
     speed: ${output.speed.slow}
+    timeout: ${output.timeout.slow}

commit 19404ff78208b091b987e4756a9cf6aa03e7f654

Fix timeout unit in ScrollUntilVisibleCommand
The `timeout` in ScrollUntilVisibleCommand should be interpreted as milliseconds: https://maestro.mobile.dev/api-reference/commands/scrolluntilvisible

Fixes https://github.com/mobile-dev-inc/maestro/issues/2108.
This was broken by https://github.com/mobile-dev-inc/maestro/pull/2023.

maestro-orchestra-models/src/main/java/maestro/orchestra/Commands.kt
@@ -115,8 +115,7 @@ data class ScrollUntilVisibleCommand(
     }
 
     private fun String.timeoutToMillis(): String {
-        val timeout = if (this.toLong() < 0) { DEFAULT_TIMEOUT_IN_MILLIS.toLong() * 1000L } else this.toLong() * 1000L
-        return timeout.toString()
+        return if (this.toLong() < 0) { DEFAULT_TIMEOUT_IN_MILLIS } else this
     }
 
     override fun description(): String {
@@ -132,7 +131,7 @@ data class ScrollUntilVisibleCommand(
     }
 
     companion object {
-        const val DEFAULT_TIMEOUT_IN_MILLIS = "20"
+        const val DEFAULT_TIMEOUT_IN_MILLIS = "20000"
         const val DEFAULT_SCROLL_DURATION = "40"
         const val DEFAULT_ELEMENT_VISIBILITY_PERCENTAGE = 100
         const val DEFAULT_CENTER_ELEMENT = false

maestro-orchestra/src/test/java/maestro/orchestra/yaml/YamlCommandReaderTest.kt
@@ -433,7 +433,7 @@ internal class YamlCommandReaderTest {
             ScrollUntilVisibleCommand(
                 selector = ElementSelector(textRegex = "Footer"),
                 direction = ScrollDirection.DOWN,
-                timeout = "20",
+                timeout = "20000",
                 scrollDuration = "40",
                 visibilityPercentage = 100,
                 label = "Scroll to the bottom",

maestro-test/src/test/resources/117_scroll_until_visible_speed.js
@@ -3,7 +3,7 @@ output.speed = {
 }
 
 output.timeout = {
-    slow: 20
+    slow: 20000
 }
 
 output.element = {