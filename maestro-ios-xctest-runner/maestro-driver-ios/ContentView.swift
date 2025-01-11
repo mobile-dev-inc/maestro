0df112bd736dc149b697f5450d42b7c7bd3391cf
Max Phillips <max.phillips@nine.com.au>
2025-01-12 06:22:48 +1100

    fix asset paths and remove unsupported swiftui feat

--- maestro-ios-xctest-runner/maestro-driver-ios/ContentView.swift
+++ maestro-ios-xctest-runner/maestro-driver-ios/ContentView.swift
@@ -10,9 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
